b656f20f14da5554d716f9ed471befbfe3fb6d20
Fufezan Mihai <fufexan@pm.me>
2021-02-12 21:49:16 +0200

    packages.nix: add programs
    - program list: less, steam, zsh
    - update packages
    - remove empty lines

--- packages.nix
+++ packages.nix
@@ -9,72 +9,75 @@
     htop gotop
     git gnupg pinentry
     fzf ripgrep
-    file tree usbutils ueberzug
+    file tree usbutils pciutils ueberzug glxinfo gnome3.zenity
     pass
     nix-index
-
     # archive tools
     p7zip unzip unrar
-
     # audio control
-    pavucontrol
-
+    pavucontrol pulsemixer
     # browser
     firefox
-
-    # C/C++ development
-    gcc gdb gnumake
-
     # document viewers and editors
     zathura libreoffice
-
-    # file managers
-    ranger lf
-
+    # file manager
+    ranger
     # file transfer and download
     curl rsync wget youtube-dl
-
     # games
     lutris steam steam-run
-
     # LaTeX
     texlive.combined.scheme-basic
-
     # media transcoding
     ffmpeg
-
     # messaging
-    tdesktop discord
-
+    tdesktop discord element-desktop
     # mouse configuration
     piper
-
     # music and video
     mpd mpdris2 ncmpcpp
     mpv playerctl
-
     # night light
     redshift
-    
-    # phone sync
-    kdeconnect
-
     # terminal emulator
     alacritty
-
     # themes
-    capitaine-cursors kdeFrameworks.breeze-icons
-    gnome-themes-extra gtk-engine-murrine
-    qgnomeplatform lxappearance
+    capitaine-cursors papirus-icon-theme
+    lxappearance
     pywal
   ];
 
   # programs configuration
+  programs.adb.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
   };
-
-  # enable ADB
-  programs.adb.enable = true;
+  programs.less.enable = true;
+  programs.steam.enable = true;
+  programs.zsh = {
+    enable = true;
+    # plugins
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    promptInit = ''
+      autoload -U promptinit && promptinit && prompt walters && setopt prompt_sp
+      PS1="%B%F{yellow}%n%F{green}@%F{blue}%M %F{magenta}%~ %(?.%F{green}%#.%F{red}%#)%f "
+    '';
+    # options
+    setOptions = [
+      "AUTO_CD"
+      "GLOB_COMPLETE"
+      "HIST_IGNORE_ALL_DUPS"
+      "HIST_REDUCE_BLANKS"
+      "INC_APPEND_HISTORY"
+      "NO_CASE_GLOB"
+    ];
+    shellAliases = {
+      grep = "grep --color";
+      l = "ls -lhAL";
+      md = "mkdir -p";
+    };
+    histFile = "$HOME/.cache/.histfile";
+  };
 }
