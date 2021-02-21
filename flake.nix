b939f266a450a333488e680f4b56dc15bf1bd0e7
Mihai Fufezan <fufexan@pm.me>
2021-02-22 00:19:22 +0200

    flake.nix: add HM and pipewire as inputs
    - pass inputs to modules

--- flake.nix
+++ flake.nix
@@ -1,8 +1,24 @@
 {
   description = "Introduction to Nix Flakes";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
+    # latest pipewire
+    pipewire = {
+      url = "git+https://gitlab.freedesktop.org/pipewire/pipewire";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, pipewire, ... }@inputs: {
     nixosConfigurations.nixpc = nixpkgs.lib.nixosSystem {
+      # extraArgs passes whatever's in the set to the modules
+      extraArgs = { inherit inputs; };
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
