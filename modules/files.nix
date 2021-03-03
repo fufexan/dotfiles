7dd6d11bf54050d00c65bf8704dce8669fdb6225
Mihai Fufezan <fufexan@pm.me>
2021-03-03 17:37:14 +0200

    config: add ranger and youtube-dl

--- modules/files.nix
+++ modules/files.nix
@@ -2,35 +2,49 @@
 
 # manage home.files
 
+let
+  home = config.home.homeDirectory;
+in
 {
   # font for rofi applets
   home.file.featherfont = {
-    source = ./config/fonts/feather.ttf;
-    target = "${config.home.homeDirectory}/.local/share/fonts/feather.ttf";
-  };
-  # flameshot config file (maybe it should be included in services.flameshot)
-  home.file."flameshot.ini" = {
-    source = ./config/flameshot.ini;
-    target = "${config.home.homeDirectory}/.config/flameshot/flameshot.ini";
-  };
-  # script to dynamically modify bspwm borders and gaps
-  home.file.dynamic_bspwm = {
-    source = ./scripts/dynamic_bspwm.sh;
-    target = "${config.home.homeDirectory}/.local/bin/dynamic_bspwm.sh"
-  };
-  # script to screenshot the monitor which contains the mouse
-  home.file.maim_monitor = {
-    source = ./scripts/maim_monitor.sh;
-    target = "${config.home.homeDirectory}/.local/bin/maim_monitor.sh";
+    source = ../config/fonts/feather.ttf;
+    target = "${home}/.local/share/fonts/feather.ttf";
   };
   # directory of different layouts for rofi applets
   home.file.rofiLayouts = {
-    source = ./config/rofi/layouts;
-    target = "${config.home.homeDirectory}/.local/share/rofi/layouts";
+    source = ../config/rofi/layouts;
+    target = "${home}/.local/share/rofi/layouts";
+  };
+
+  # config files
+  home.file."flameshot.ini" = {
+    source = ../config/flameshot.ini;
+    target = "${home}/.config/flameshot/flameshot.ini";
+  };
+  home.file.rangerrc = {
+    source = ../config/ranger.rc.conf;
+    target = "${home}/.config/ranger/rc.conf";
+  };
+  home.file.youtube-dl = {
+    source = ../config/youtube-dl.conf;
+    target = "${home}/.config/youtube-dl/config";
+  };
+  
+  # scripts
+  # script to dynamically modify bspwm borders and gaps
+  home.file.dynamic_bspwm = {
+    source = ../scripts/dynamic_bspwm.sh;
+    target = "${home}/.local/bin/dynamic_bspwm.sh";
+  };
+  # script to screenshot the monitor which contains the mouse
+  home.file.maim_monitor = {
+    source = ../scripts/maim_monitor.sh;
+    target = "${home}/.local/bin/maim_monitor.sh";
   };
   # rofi applets scripts
   home.file.rofiScripts = {
-    source = ./scripts/rofi;
-    target = "${config.home.homeDirectory}/.local/bin/rofi";
+    source = ../scripts/rofi;
+    target = "${home}/.local/bin/rofi";
   };
 }
