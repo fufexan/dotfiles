f5d9a8be2f67f590f3bb104ec6852c71b6a36630
Mihai Fufezan <fufexan@pm.me>
2021-03-17 23:07:46 +0200

    lightcord: 0.1.4 -> 0.1.5

--- overlays/lightcord/default.nix
+++ overlays/lightcord/default.nix
@@ -1,12 +1,13 @@
 { fetchzip, pkgs }:
 pkgs.callPackage ./base.nix rec {
   pname = "lightcord";
-  binaryName = "Lightcord";
+  binaryName = "lightcord";
   desktopName = "Lightcord";
-  version = "0.1.4";
-  src = fetchzip {
-    url = "https://github.com/Lightcord/Lightcord/releases/download/v${version}/lightcord-linux-x64.zip";
-    sha256 = "0llpxwabd204wgvhv7pjpx8saknpg2xyb7vjw2bnwdj20nh9w78b";
+
+  version = "0.1.5";
+  src = pkgs.fetchzip {
+    url = "https://github.com/Lightcord/Lightcord/releases/download/0.1.5/lightcord-linux-x64.zip";
+    sha256 = "sha256-lorjKF7RQHLt3e57CrPa4QqpztHQqsF+ijiJD5hJYTY=";
     stripRoot = false;
   };
 }
