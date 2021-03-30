bbcd004a2831862dff8ffe9adf1339112c648162
Mihai Fufezan <fufexan@pm.me>
2021-03-30 12:29:45 +0300

    modules/pipewire: low latency part 1

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -1,10 +1,11 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
+
+# low-latency PipeWire configuration
 
 let
   pipewire = pkgs.pipewire;
   pipewirei686 = pkgs.pkgsi686Linux.pipewire;
 in {
-  # sound
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -13,23 +14,54 @@ in {
     pulse.enable = true;
 
     config = {
+      # if you need lower min-quantum
       pipewire = {
-        "context.properties" = {
-          "default.clock.rate" = 48000;
-          "default.clock.quantum" = 256;
-          "default.clock.min-quantum" = 8;
-        };
+        "context.properties" = { "default.clock.min-quantum" = 8; };
       };
+
+      # pulse clients config
       pipewire-pulse = {
-        "context.modules".libpipewire-module-protocol-pulse.args = {
-          "pulse.min.req" = "8/48000";
-          "pulse.min.quantum" = "8/48000";
-          "pulse.min.frag" = "8/48000";
+        "context.modules" = [
+          # these have to be set for some reason (or I'm just dumb)
+          { name = "libpipewire-module-protocol-native"; }
+          { name = "libpipewire-module-client-node"; }
+          { name = "libpipewire-module-adapter"; }
+          { name = "libpipewire-module-metadata"; }
+          # actual lowering of latency
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = {
+              "server.address" = [ "unix:native" ];
+              "pulse.min.req" = "8/48000";
+              "pulse.min.quantum" = "8/48000";
+              "pulse.min.frag" = "8/48000";
+            };
+          }
+        ];
+        "stream.properties" = {
+          "node.latency" = "8/48000";
         };
       };
     };
   };
 
+  # lower latency alsa format
+  environment.etc."pipewire/media-session.d/alsa-monitor.conf".text = ''
+    rules = [
+      {
+        matches = [{ node.name = ~alsa_output.*?-LOUD.* }]
+        actions = {
+          update-props = {
+            audio.format = "S16LE"
+            audio.rate = 48000
+            api.alsa.period-size = 160
+          }
+        }
+      }
+    ]
+  '';
+
+  # add pipewire as an alsa device
   environment.etc."alsa/conf.d/49-pipewire-modules.conf".text = ''
     pcm_type.pipewire {
       libs {
@@ -44,4 +76,39 @@ in {
       }
     }
   '';
+
+  # enable higher rtprio for the audio group
+  # TODO: change to rtkit or something
+  security.pam.loginLimits = [
+    {
+      domain = "@audio";
+      item = "memlock";
+      type = "-";
+      value = "unlimited";
+    }
+    {
+      domain = "@audio";
+      item = "rtprio";
+      type = "-";
+      value = "99";
+    }
+  ];
+
+  # allow the audio group to write to rtc0 and hpet
+  services.udev.extraRules = ''
+    KERNEL=="rtc0", GROUP="audio"
+    KERNEL=="hpet", GROUP="audio"
+  '';
+
+  # increase kernel timer. useful for snappier system
+  # causes fewer underruns for low sample size applications
+  systemd.services.kernelTimer = {
+    enable = true;
+    description = "Increase timer resolution";
+    script = ''
+        #!/bin/sh
+        echo 2048 | tee /sys/class/rtc/rtc0/max_user_freq /proc/sys/dev/hpet/max-user-freq
+    '';
+    wantedBy = [ "multi-user.target" ];
+  };
 }
