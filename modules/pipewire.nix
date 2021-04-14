b652d52d4f19a31024efce529a97feb0a871fd5f
Mihai Fufezan <fufexan@pm.me>
2021-04-14 16:46:10 +0300

    modules/pipewire: low latency part 2

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -1,11 +1,8 @@
-{ config, pkgs, ... }:
+{ ... }:
 
 # low-latency PipeWire configuration
 
-let
-  pipewire = pkgs.pipewire;
-  pipewirei686 = pkgs.pkgsi686Linux.pipewire;
-in {
+{
   services.pipewire = {
     enable = true;
     alsa.enable = true;
@@ -14,10 +11,10 @@ in {
     pulse.enable = true;
 
     config = {
-      # if you need lower min-quantum
-      pipewire = {
-        "context.properties" = { "default.clock.min-quantum" = 8; };
-      };
+      # if you need lower min-quantum (default is 32)
+      #pipewire = {
+      #  "context.properties" = { "default.clock.min-quantum" = 8; };
+      #};
 
       # pulse clients config
       pipewire-pulse = {
@@ -26,21 +23,21 @@ in {
           { name = "libpipewire-module-protocol-native"; }
           { name = "libpipewire-module-client-node"; }
           { name = "libpipewire-module-adapter"; }
-          { name = "libpipewire-module-metadata"; }
+          {
+            name = "libpipewire-module-metadata";
+          }
           # actual lowering of latency
           {
             name = "libpipewire-module-protocol-pulse";
             args = {
               "server.address" = [ "unix:native" ];
-              "pulse.min.req" = "8/48000";
-              "pulse.min.quantum" = "8/48000";
-              "pulse.min.frag" = "8/48000";
+              "pulse.min.req" = "32/48000";
+              "pulse.min.quantum" = "32/48000"; # 0.67ms
+              "pulse.min.frag" = "32/48000";
             };
           }
         ];
-        "stream.properties" = {
-          "node.latency" = "8/48000";
-        };
+        "stream.properties" = { "node.latency" = "32/48000"; };
       };
     };
   };
@@ -49,32 +46,22 @@ in {
   environment.etc."pipewire/media-session.d/alsa-monitor.conf".text = ''
     rules = [
       {
-        matches = [{ node.name = ~alsa_output.*?-LOUD.* }]
+        matches = [
+          # you won't get much out of it, about ~40ms in jack_iodelay
+          { node.name = "alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00.analog-stereo" }
+          # with this you'll get ~3ms
+          { node.name = "alsa_output.pci-0000_00_1b.0.analog-stereo" }
+        ]
         actions = {
           update-props = {
-            audio.format = "S16LE"
-            audio.rate = 48000
-            api.alsa.period-size = 160
+            audio.format = "S32LE"
+            audio.rate = 192000
+            api.alsa.period-size = 2
+            #api.alsa.disable-batch = true
           }
         }
       }
     ]
-  '';
-
-  # add pipewire as an alsa device
-  environment.etc."alsa/conf.d/49-pipewire-modules.conf".text = ''
-    pcm_type.pipewire {
-      libs {
-        native ${pipewire.lib}/lib/alsa-lib/libasound_module_pcm_pipewire.so
-        32Bit ${pipewirei686.lib}/lib/alsa-lib/libasound_module_pcm_pipewire.so
-      }
-    }
-    ctl_type.pipewire {
-      libs {
-        native ${pipewire.lib}/lib/alsa-lib/libasound_module_ctl_pipewire.so
-        32Bit ${pipewirei686.lib}/lib/alsa-lib/libasound_module_ctl_pipewire.so
-      }
-    }
   '';
 
   # enable higher rtprio for the audio group
@@ -106,8 +93,8 @@ in {
     enable = true;
     description = "Increase timer resolution";
     script = ''
-        #!/bin/sh
-        echo 2048 | tee /sys/class/rtc/rtc0/max_user_freq /proc/sys/dev/hpet/max-user-freq
+      #!/bin/sh
+      echo 2048 | tee /sys/class/rtc/rtc0/max_user_freq /proc/sys/dev/hpet/max-user-freq
     '';
     wantedBy = [ "multi-user.target" ];
   };
