bd7b54c46eca0aa01833ce43e2e212f1b3a11070
Mihai Fufezan <fufexan@pm.me>
2021-04-16 10:49:25 +0300

    home/modules/shell: add user and root systemctl alias

--- home/modules/shell.nix
+++ home/modules/shell.nix
@@ -36,67 +36,67 @@
       path = "${config.xdg.dataHome}/zsh_history";
     };
     initExtra = ''
-      # autoloads
-      autoload -U history-search-end
-      autoload -Uz vcs_info
-      autoload edit-command-line
+            # autoloads
+            autoload -U history-search-end
+            autoload -Uz vcs_info
+            autoload edit-command-line
 
-      # configure prompt
-      PS1="%B%F{yellow}%n%F{green}@%F{blue}%M %F{magenta}%~ %(?.%F{green}%#.%F{red}%#)%f "
+            # configure prompt
+            PS1="%B%F{yellow}%n%F{green}@%F{blue}%M %F{magenta}%~ %(?.%F{green}%#.%F{red}%#)%f "
 
-      # search history based on what's typed in the prompt
-      # group functions
-      zle -N history-beginning-search-backward-end history-search-end
-      zle -N history-beginning-search-forward-end history-search-end
-      # bind functions to up and down arrow keys
-      bindkey "^[[A" history-beginning-search-backward-end
-      bindkey "^[[B" history-beginning-search-forward-end
+            # search history based on what's typed in the prompt
+            # group functions
+            zle -N history-beginning-search-backward-end history-search-end
+            zle -N history-beginning-search-forward-end history-search-end
+            # bind functions to up and down arrow keys
+            bindkey "^[[A" history-beginning-search-backward-end
+            bindkey "^[[B" history-beginning-search-forward-end
 
-      # allow editing the command in $EDITOR with ^E
-      zle -N edit-command-line
-      bindkey '^e' edit-command-line
+            # allow editing the command in $EDITOR with ^E
+            zle -N edit-command-line
+            bindkey '^e' edit-command-line
 
-      # change cursor shape for different vi modes
-      function zle-keymap-select {
-        if [[ $KEYMAP == vicmd ]] ||
-           [[ $1 = 'block' ]]; then
-          echo -ne '\e[1 q'
-        elif [[ $KEYMAP == main ]] ||
-             [[ $KEYMAP == viins ]] ||
-             [[ $KEYMAP = '\' ]] ||
-             [[ $1 = 'beam' ]]; then
-          echo -ne '\e[5 q'
-        fi
-      }
-      zle -N zle-keymap-select
-      zle-line-init() {
-        echo -ne "\e[5 q"
-      }
-      zle -N zle-line-init
-      echo -ne '\e[5 q' # Use beam shape cursor on startup.
-      preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+            # change cursor shape for different vi modes
+            function zle-keymap-select {
+              if [[ $KEYMAP == vicmd ]] ||
+                 [[ $1 = 'block' ]]; then
+                echo -ne '\e[1 q'
+              elif [[ $KEYMAP == main ]] ||
+                   [[ $KEYMAP == viins ]] ||
+                   [[ $KEYMAP = '\' ]] ||
+                   [[ $1 = 'beam' ]]; then
+                echo -ne '\e[5 q'
+              fi
+            }
+            zle -N zle-keymap-select
+            zle-line-init() {
+              echo -ne "\e[5 q"
+            }
+            zle -N zle-line-init
+            echo -ne '\e[5 q' # Use beam shape cursor on startup.
+            preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
 
-      # git integration
-      autoload -Uz vcs_info
-      precmd_vcs_info() { vcs_info }
-      precmd_functions+=( precmd_vcs_info )
-      setopt prompt_subst
-      RPROMPT=\$vcs_info_msg_0_
-      zstyle ':vcs_info:git:*' formats '%F{blue}(%b)%r%f'
-      zstyle ':vcs_info:*' enable git
+            # git integration
+            autoload -Uz vcs_info
+            precmd_vcs_info() { vcs_info }
+            precmd_functions+=( precmd_vcs_info )
+            setopt prompt_subst
+            RPROMPT=\$vcs_info_msg_0_
+            zstyle ':vcs_info:git:*' formats '%F{blue}(%b)%r%f'
+            zstyle ':vcs_info:*' enable git
 
-      # case insensitive tab completion
-      zstyle ':completion:*' completer _complete _ignored _approximate
-      zstyle ':completion:*' list-colors '\'
-      zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-      zstyle ':completion:*' menu select
-      zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-      zstyle ':completion:*' verbose true
-      _comp_options+=(globdots)
+            # case insensitive tab completion
+            zstyle ':completion:*' completer _complete _ignored _approximate
+            zstyle ':completion:*' list-colors '\'
+            zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+            zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+            zstyle ':completion:*' menu select
+            zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+            zstyle ':completion:*' verbose true
+            _comp_options+=(globdots)
 
-			${builtins.readFile ./nix-completions.sh}
-    '';
+      			${builtins.readFile ./nix-completions.sh}
+          '';
     shellAliases = {
       grep = "grep --color";
       ip = "ip --color";
@@ -117,6 +117,8 @@
       gl = "git log";
       gs = "git status --short";
       gss = "git status";
+      us = "systemctl --user";
+      rs = "doas systemctl";
     };
     shellGlobalAliases = { exa = "exa --icons --git"; };
   };
