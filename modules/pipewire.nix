0f62a98c3bdf5c8c95bc1e36f0013cb68ff24fda
Mihai Fufezan <fufexan@pm.me>
2021-05-06 21:38:07 +0300

    modules/pipewire: low latency part 3

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -11,91 +11,53 @@
     pulse.enable = true;
 
     config = {
-      # if you need lower min-quantum (default is 32)
-      #pipewire = {
-      #  "context.properties" = { "default.clock.min-quantum" = 8; };
-      #};
-
       # pulse clients config
       pipewire-pulse = {
+        "context.properties" = { };
         "context.modules" = [
-          # these have to be set for some reason (or I'm just dumb)
+          {
+            name = "libpipewire-module-rtkit";
+            args = {
+              "nice.level" = -15;
+              "rt.prio" = 88;
+              "rt.time.soft" = 200000;
+              "rt.time.hard" = 200000;
+            };
+            flags = [ "ifexists" "nofail" ];
+          }
           { name = "libpipewire-module-protocol-native"; }
           { name = "libpipewire-module-client-node"; }
           { name = "libpipewire-module-adapter"; }
-          {
-            name = "libpipewire-module-metadata";
-          }
-          # actual lowering of latency
+          { name = "libpipewire-module-metadata"; }
           {
             name = "libpipewire-module-protocol-pulse";
             args = {
-              "server.address" = [ "unix:native" ];
               "pulse.min.req" = "32/48000";
-              "pulse.min.quantum" = "32/48000"; # 0.67ms
+              "pulse.min.quantum" = "32/48000";
               "pulse.min.frag" = "32/48000";
+              "server.address" = [ "unix:native" ];
             };
           }
         ];
-        "stream.properties" = { "node.latency" = "32/48000"; };
+
+        "stream.properties" = {
+          "node.latency" = "32/48000";
+          "resample.quality" = 1;
+        };
       };
     };
     # lower latency alsa format
     media-session.config.alsa-monitor = {
       rules = [{
-        matches = [
-          # you won't get much out of it, about ~40ms in jack_iodelay
-          {
-            node.name =
-              "alsa_output.usb-Kingston_HyperX_Virtual_Surround_Sound_00000000-00.analog-stereo";
-          }
-          # with this you'll get ~3ms
-          { node.name = "alsa_output.pci-0000_00_1b.0.analog-stereo"; }
-        ];
+        matches = [{ node.name = "alsa_output.*"; }];
         actions = {
           update-props = {
             "audio.format" = "S32LE";
-            "audio.rate" = 192000;
-            "api.alsa.period-size" = 2;
-            #"api.alsa.disable-batch" = true;
+            "audio.rate" = 96000;
+            "api.alsa.period-size" = 32;
           };
         };
       }];
     };
-  };
-
-  # enable higher rtprio for the audio group
-  # TODO: change to rtkit or something
-  security.pam.loginLimits = [
-    {
-      domain = "@audio";
-      item = "memlock";
-      type = "-";
-      value = "unlimited";
-    }
-    {
-      domain = "@audio";
-      item = "rtprio";
-      type = "-";
-      value = "99";
-    }
-  ];
-
-  # allow the audio group to write to rtc0 and hpet
-  services.udev.extraRules = ''
-    KERNEL=="rtc0", GROUP="audio"
-    KERNEL=="hpet", GROUP="audio"
-  '';
-
-  # increase kernel timer. useful for snappier system
-  # causes fewer underruns for low sample size applications
-  systemd.services.kernelTimer = {
-    enable = true;
-    description = "Increase timer resolution";
-    script = ''
-      #!/bin/sh
-      echo 2048 | tee /sys/class/rtc/rtc0/max_user_freq /proc/sys/dev/hpet/max-user-freq
-    '';
-    wantedBy = [ "multi-user.target" ];
   };
 }
