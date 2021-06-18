e2ddf2d30c031d1e5f4469084610d5397a1e97bd
Mihai Fufezan <fufexan@pm.me>
2021-06-18 22:26:33 +0300

    home/modules/x11/polybar: separate pulse module
    it's now possible to right click the module to start up pavucontrol

--- home/modules/x11/polybar/default.nix
+++ home/modules/x11/polybar/default.nix
@@ -13,11 +13,26 @@ let
     exec = ${mprisScript}/bin/mpris
     tail = true
 
-    label-maxlen = 60
+    label-maxlen = 100
 
     interval = 2
-    format =  <label>
+    format = <label>
     format-padding = 2
+  '';
+  pulseModule = ''
+    [module/pulseaudio]
+    type = internal/pulseaudio
+    sink = 
+    use-ui-max = false
+    interval = 5
+    click-right = ${pkgs.pavucontrol}/bin/pavucontrol
+    format-volume = <ramp-volume> <label-volume>
+    label-muted = 󰸈 muted
+    label-muted-foreground = #66
+
+    ramp-volume-0 = 󰕿
+    ramp-volume-1 = 󰖀
+    ramp-volume-2 = 󰕾
   '';
 in
 {
@@ -25,7 +40,7 @@ in
     enable = true;
     package = poly;
     config = ./config.ini;
-    extraConfig = mprisModule;
+    extraConfig = mprisModule + pulseModule;
     script = ''
       polybar main &
     '';
