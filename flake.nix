6041c223bc8c267f6e2546b2ae7a69a8f769a2f0
Mihai Fufezan <fufexan@pm.me>
2021-07-17 21:10:26 +0300

    flake.nix: switch hm back to github link

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     # flakes
     agenix.url = "github:ryantm/agenix";
     hm = {
-      url = path:../git/home-manager; #"github:nix-community/home-manager";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
