a1543e6c3beef400fe62bf83db5ad2396ea4cde7
Mihai Fufezan <fufexan@pm.me>
2021-08-19 11:30:31 +0300

    modules/minimal: add nix-community cache

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -61,11 +61,13 @@
       "https://cache.nixos.org"
       "https://fufexan.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     binaryCachePublicKeys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
     extraOptions = ''
