a7b20a2c99d3da9b21ce86f20afe350d206f0945
Mihai Fufezan <fufexan@pm.me>
2021-08-20 20:54:27 +0300

    emacs: add packages

--- home/editors/emacs/default.nix
+++ home/editors/emacs/default.nix
@@ -6,11 +6,24 @@
     package = inputs.nobbz.packages.x86_64-linux.emacsPgtkGcc;
 
     extraPackages = epkgs: with epkgs; [
+      # themes
+      horizon-theme
+
+      # language modes
+      markdown-mode
+      nix-mode
+
+      # QoL
+      consult
       editorconfig
       eglot
-      horizon-theme
-      nix-mode
+      magit
+      multiple-cursors
+      nixpkgs-fmt
       phi-autopair
+      treemacs
+      undo-tree
+      vertico
     ];
   };
 }
