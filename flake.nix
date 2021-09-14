62457664d868a21c6b22dd6ff2ca07e4c5bcb861
Mihai Fufezan <fufexan@pm.me>
2021-09-14 15:24:38 +0300

    flake.nix: refactor
    - move inputs at the bottom
    - move hm modules in let binding, prevent duplication
    - simplify homeConfigurations

--- flake.nix
+++ flake.nix
@@ -1,11 +1,122 @@
 {
   description = "fufexan's NixOS and Home-Manager flake";
 
+  outputs = { self, utils, nixpkgs, ... }@inputs:
+    let
+      hmModules = {
+        desktop = [
+          ./home
+          ./home/profiles/mihai-kiiro
+          ./home/files.nix
+          ./home/games.nix
+          ./home/media.nix
+          ./home/nix.nix
+          ./home/x11
+          ./home/editors/emacs/doom.nix
+          ./home/editors/kakoune
+          ./home/editors/neovim
+          inputs.nix-doom-emacs.hmModule
+        ];
+      };
+    in
+    utils.lib.mkFlake
+      {
+        inherit self inputs;
+
+        # apply overlays to nixpkgs
+        channels.nixpkgs.overlaysBuilder = channels: [
+          (final: prev: { inherit (channels.nixpkgs-kak) kakounePlugins; })
+          self.overlay
+          inputs.devshell.overlay
+          inputs.powercord.overlay
+          inputs.utils.overlay
+        ];
+        channelsConfig.allowUnfree = true;
+
+        # modules and hosts
+        hosts = {
+          homesv.modules = [
+            ./hosts/homesv
+            inputs.snm.nixosModule
+            { home-manager.users.mihai = import ./home/cli.nix; }
+          ];
+
+          tosh.modules = [
+            ./hosts/tosh
+            ./modules/desktop.nix
+            { home-manager.users.mihai.imports = hmModules.desktop; }
+          ];
+
+          kiiro.modules = [
+            ./hosts/kiiro
+            ./modules/desktop.nix
+            ./modules/gnome.nix
+            { home-manager.users.mihai.imports = hmModules.desktop; }
+          ];
+        };
+
+        hostDefaults.modules = [
+          ./modules/minimal.nix
+          ./modules/security.nix
+          inputs.agenix.nixosModules.age
+          inputs.hm.nixosModule
+          inputs.kmonad.nixosModule
+          inputs.nix-gaming.nixosModule
+          {
+            home-manager = {
+              extraSpecialArgs = { inherit inputs self; };
+              useGlobalPkgs = true;
+            };
+          }
+        ];
+
+        # homes
+        homeConfigurations =
+          let
+            configuration = { };
+            extraSpecialArgs = { inherit inputs self; };
+            generateHome = inputs.hm.lib.homeManagerConfiguration;
+            homeDirectory = "/home/${username}";
+            pkgs = self.pkgs.${system}.nixpkgs;
+            system = "x86_64-linux";
+            username = "mihai";
+          in
+          {
+            # homeConfigurations
+            cli = generateHome {
+              inherit system username homeDirectory extraSpecialArgs pkgs configuration;
+              extraModules = [ ./home/cli.nix ];
+            };
+
+            "mihai@kiiro" = generateHome {
+              inherit system username homeDirectory extraSpecialArgs pkgs configuration;
+              extraModules = hmModules.desktop;
+            };
+
+            "mihai@tosh" = generateHome {
+              inherit system username homeDirectory extraSpecialArgs pkgs configuration;
+              extraModules = hmModules.tosh;
+            };
+          };
+
+        # overlays
+        overlay = import ./pkgs { inherit inputs; };
+        overlays = utils.lib.exportOverlays { inherit (self) pkgs inputs; };
+
+        # packages
+        outputsBuilder = channels: {
+          packages = utils.lib.exportPackages self.overlays channels;
+          devShell = channels.nixpkgs.devshell.mkShell {
+            packages = with channels.nixpkgs; [ nixpkgs-fmt rnix-lsp ];
+            name = "dots";
+          };
+        };
+      };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixpkgs-kak.url = "github:NixOS/nixpkgs/e5920f73965ce9fd69c93b9518281a3e8cb77040";
 
-    fu.url = "github:numtide/flake-utils";
     utils = {
       url = "github:gytis-ivaskevicius/flake-utils-plus/release-1.2.0-without-deprecated-code";
       inputs.flake-utils.follows = "fu";
@@ -15,6 +126,8 @@
     agenix.url = "github:ryantm/agenix";
 
     devshell.url = "github:numtide/devshell";
+
+    fu.url = "github:numtide/flake-utils";
 
     hm = {
       url = "github:nix-community/home-manager";
@@ -71,170 +184,4 @@
     picom = { url = "github:yshui/picom"; flake = false; };
     vim-horizon = { url = "github:ntk148v/vim-horizon"; flake = false; };
   };
-
-  outputs = { self, utils, nixpkgs, ... }@inputs:
-    utils.lib.mkFlake {
-      inherit self inputs;
-
-
-      # channel setup
-
-      # apply overlays to nixpkgs
-      channels.nixpkgs.overlaysBuilder = channels: [
-        (
-          final: prev: {
-            inherit (channels.nixpkgs-kak) kakounePlugins;
-          }
-        )
-        self.overlay
-        inputs.devshell.overlay
-        inputs.powercord.overlay
-        inputs.utils.overlay
-      ];
-
-      channelsConfig = { allowUnfree = true; };
-
-
-      # modules and hosts
-
-      nixosModules = utils.lib.exportModules [
-        ./modules/desktop.nix
-        ./modules/minimal.nix
-        ./modules/security.nix
-      ];
-
-      hostDefaults.modules = [
-        self.nixosModules.minimal
-        self.nixosModules.security
-        inputs.agenix.nixosModules.age
-        inputs.hm.nixosModule
-        {
-          home-manager = {
-            extraSpecialArgs = { inherit inputs self; };
-            useGlobalPkgs = true;
-          };
-        }
-        inputs.kmonad.nixosModule
-      ];
-
-      hosts = {
-        homesv.modules = with self.nixosModules; [
-          ./hosts/homesv
-          inputs.snm.nixosModule
-          { home-manager.users.mihai = import ./home/cli.nix; }
-        ];
-
-        tosh.modules = with self.nixosModules; [
-          ./hosts/tosh
-          desktop
-          inputs.nix-gaming.nixosModule
-          { home-manager.users.mihai = import ./home; }
-        ];
-
-        kiiro.modules = with self.nixosModules; [
-          ./hosts/kiiro
-          desktop
-          ./modules/gnome.nix
-          inputs.nix-gaming.nixosModule
-          {
-            home-manager.users.mihai.imports = [
-              ./home
-              ./home/profiles/mihai-kiiro
-              ./home/files.nix
-              ./home/games.nix
-              ./home/media.nix
-              ./home/nix.nix
-              ./home/x11
-              ./home/editors/emacs/doom.nix
-              ./home/editors/kakoune
-              ./home/editors/neovim
-              inputs.nix-doom-emacs.hmModule
-            ];
-          }
-        ];
-      };
-
-
-      # homes
-
-      homeConfigurations =
-        let
-          username = "mihai";
-          homeDirectory = "/home/mihai";
-          system = "x86_64-linux";
-          extraSpecialArgs = { inherit inputs self; };
-          generateHome = inputs.hm.lib.homeManagerConfiguration;
-          nixpkgs = {
-            config = { allowUnfree = true; };
-            overlays = [
-              inputs.utils.overlay
-              inputs.powercord.overlay
-            ];
-          };
-        in
-        {
-          # homeConfigurations
-          cli = generateHome {
-            inherit system username homeDirectory extraSpecialArgs;
-            configuration = {
-              imports = [ ./home/cli.nix ];
-              inherit nixpkgs;
-            };
-          };
-
-          "mihai@kiiro" = generateHome {
-            inherit system username homeDirectory extraSpecialArgs;
-            pkgs = self.pkgs.x86_64-linux.nixpkgs;
-            configuration = {
-              imports = [ ./home ];
-              inherit nixpkgs;
-            };
-            extraModules = [
-              ./home/profiles/mihai-kiiro
-              ./home/files.nix
-              ./home/games.nix
-              ./home/media.nix
-              ./home/nix.nix
-              ./home/mail.nix
-              ./home/x11
-              ./home/editors/emacs
-              ./home/editors/kakoune
-              ./home/editors/neovim
-            ];
-          };
-
-          "mihai@tosh" = generateHome {
-            inherit system username homeDirectory extraSpecialArgs;
-            pkgs = self.pkgs.x86_64-linux.nixpkgs;
-            configuration = {
-              imports = [ ./home ];
-              inherit nixpkgs;
-            };
-            extraModules = [
-              ./home/profiles/mihai-tosh
-              ./home/files.nix
-              ./home/media.nix
-              ./home/nix.nix
-              ./home/x11
-              ./home/editors/emacs
-              ./home/editors/kakoune
-            ];
-          };
-        };
-
-      # overlays
-      overlay = import ./pkgs { inherit inputs; };
-      overlays = utils.lib.exportOverlays {
-        inherit (self) pkgs inputs;
-      };
-
-      # packages
-      outputsBuilder = channels: {
-        packages = utils.lib.exportPackages self.overlays channels;
-        devShell = channels.nixpkgs.mkShell {
-          packages = with channels.nixpkgs; [ nixpkgs-fmt rnix-lsp ];
-          name = "dots";
-        };
-      };
-    };
 }
