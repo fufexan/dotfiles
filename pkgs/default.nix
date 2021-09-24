48454bbd69d900a025380e612fec00338bbbf259
Mihai Fufezan <fufexan@pm.me>
2021-09-25 02:18:50 +0300

    flake.lock: update inputs
    rnix-lsp: fixed on master

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -26,21 +26,6 @@ final: prev: {
     }
   );
 
-  # currently based on https://github.com/Ma27/rnix-lsp/tree/experimental.
-  rnix-lsp = with final; rustPlatform.buildRustPackage {
-    pname = "rnix-lsp-unstable";
-    version = "2021-09-18";
-    src = fetchFromGitHub {
-      owner = "ma27";
-      repo = "rnix-lsp";
-      rev = "2136b1b7ca0f387451f34747609b21064220fd0a";
-      sha256 = "sha256-IiJ8Sx0VEo/VmQPg/8jqAHttm1P+qLi2t1fQ3AJAbog=";
-    };
-    cargoPatches = [ ./rnix-fix.patch ];
-    cargoSha256 = "sha256-yG9w7/RWKyPIqKpy9ULsd5MTsvjkaPBSH/bKfpYMu3U=";
-    checkInputs = [ nixUnstable ];
-  };
-
   orchis-theme = prev.callPackage ./orchis-theme { };
 
   picom = prev.picom.overrideAttrs (
