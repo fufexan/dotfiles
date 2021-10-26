e12861afd3f595e2f6887ec60afe140308a73ca5
Mihai Fufezan <fufexan@pm.me>
2021-10-26 03:21:33 +0300

    xwayland: fix latency

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -40,4 +40,10 @@ final: prev: {
   };
 
   technic-launcher = prev.callPackage ./technic.nix { };
+
+  xwayland = prev.xwayland.overrideAttrs ({ patches ? [ ], ... }: {
+    preConfigure = ''
+      patch -p1 < ${./xwayland.patch}
+    '';
+  });
 }
