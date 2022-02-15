f9e66749c57021f5bc95ff5ce5b8132a6cde7333
Mihai Fufezan <fufexan@pm.me>
2022-02-15 13:40:10 +0200

    waveform: init

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,8 @@
 final: prev: {
   technic-launcher = prev.callPackage ./technic.nix { };
 
+  waveform = prev.callPackage ./waveform { };
+
   xwayland = prev.xwayland.overrideAttrs ({ patches ? [ ], ... }: {
     preConfigure = ''
       patch -p1 < ${./patches/xwayland.patch}
