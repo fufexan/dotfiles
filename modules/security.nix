f58a2f67ecef97d60a969323ebeca1d1287dab23
Mihai Fufezan <fufexan@protonmail.com>
2022-07-01 02:24:52 +0300

    modules/security: remove tmpfs options

--- modules/security.nix
+++ modules/security.nix
@@ -5,18 +5,6 @@
 }:
 # security tweaks borrowed from @hlissner
 {
-  ## System security tweaks
-  # Prevent replacing the running kernel w/o reboot
-  # Also prevents hibernation
-  #security.protectKernelImage = true;
-
-  # tmpfs = /tmp is mounted in ram. Doing so makes temp file management speedy
-  # on ssd systems, and volatile! Because it's wiped on reboot.
-  boot.tmpOnTmpfs = lib.mkDefault true;
-  # If not using tmpfs, which is naturally purged on reboot, we must clean it
-  # /tmp ourselves. /tmp should be volatile storage!
-  boot.cleanTmpDir = lib.mkDefault (!config.boot.tmpOnTmpfs);
-
   # Fix a security hole in place for backwards compatibility. See desc in
   # nixpkgs/nixos/modules/system/boot/loader/systemd-boot/systemd-boot.nix
   boot.loader.systemd-boot.editor = false;
