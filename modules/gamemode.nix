a60e392271987845d6e06e9b7946817b158bd69e
Mihai Fufezan <fufexan@protonmail.com>
2022-07-29 18:56:02 +0300

    modules/gamemode: fix start/end scripts

--- modules/gamemode.nix
+++ modules/gamemode.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: let
-  start = ''
+  startscript = pkgs.writeShellScript "gamemode-start" ''
     hyprctl --batch 'keyword decoration:blur 0 ; keyword animations:enabled 0 ; misc:no_vfr 1'
     dunstctl set-paused true
     systemctl --user stop easyeffects
   '';
 
-  end = ''
+  endscript = pkgs.writeShellScript "gamemode-end" ''
     hyprctl --batch 'keyword decoration:blur 1 ; keyword animations:enabled 1 ; misc:no_vfr 0'
     dunstctl set-paused false
     systemctl --user start easyeffects
@@ -18,7 +18,10 @@ in {
         softrealtime = "auto";
         renice = 15;
       };
-      custom = {inherit start end;};
+      custom = {
+        start = "${startscript}";
+        end = "${endscript}";
+      };
     };
   };
 }
