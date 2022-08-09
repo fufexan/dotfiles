4787af4b19ad2f7b156e34664c9fb05df38e9046
Mihai Fufezan <fufexan@protonmail.com>
2022-08-10 01:36:46 +0300

    spicetify: update

--- home/spicetify.nix
+++ home/spicetify.nix
@@ -3,45 +3,26 @@
   lib,
   ...
 }: {
-  programs.spicetify = let
-    hidePodcasts = pkgs.fetchFromGitHub {
-      owner = "theRealPadster";
-      repo = "spicetify-hide-podcasts";
-      rev = "cfda4ce0c3397b0ec38a971af4ff06daba71964d";
-      sha256 = "146bz9v94dk699bshbc21yq4y5yc38lq2kkv7w3sjk4x510i0v3q";
-    };
-
-    catppuccin = pkgs.fetchFromGitHub {
-      owner = "catppuccin";
-      repo = "spicetify";
-      rev = "8aaacc4b762fb507b3cf7d4d1b757eb849fcbb52";
-      sha256 = "sha256-yfRbYM065wRJqaZB+SsOZi4+e2+ZOD0iKyhNVBJcrqA=";
-    };
-
-    variants = ["catppuccin-latte" "catppuccin-frappe" "catppuccin-macchiato" "catppuccin-mocha"];
-  in {
+  programs.spicetify = {
     enable = true;
+
+    spicetifyPackage = pkgs.spicetify-cli.overrideAttrs (oa: rec {
+      pname = "spicetify-cli";
+      version = "2.9.9";
+      src = pkgs.fetchgit {
+        url = "https://github.com/spicetify/${pname}";
+        rev = "v${version}";
+        sha256 = "1a6lqp6md9adxjxj4xpxj0j1b60yv3rpjshs91qx3q7blpsi3z4z";
+      };
+    });
 
     theme = "catppuccin-mocha";
     colorScheme = "flamingo";
-
-    injectCss = true;
-    replaceColors = true;
-    overwriteAssets = true;
 
     enabledExtensions = [
       "fullAppDisplay.js"
       "shuffle+.js"
       "hidePodcasts.js"
-      "catppuccin-mocha.js"
     ];
-
-    thirdPartyExtensions =
-      {
-        hidePodcasts = "${hidePodcasts}/hidePodcasts.js";
-      }
-      // lib.genAttrs variants (n: "${catppuccin}/js/${n}.js");
-
-    thirdPartyThemes = lib.genAttrs variants (n: "${catppuccin}/${n}");
   };
 }
