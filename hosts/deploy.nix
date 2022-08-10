6e348e570b4fb587ecac163a84dcd85dc00ab9f7
Mihai Fufezan <fufexan@protonmail.com>
2022-08-10 23:46:51 +0300

    hosts: remove unused hosts

--- hosts/deploy.nix
+++ hosts/deploy.nix
@@ -6,8 +6,6 @@ inputs: {
         user = "root";
         path = inputs.deploy-rs.lib.aarch64-linux.activate.nixos inputs.self.nixosConfigurations.arm-server;
       };
-      sshOpts = ["-i" "/etc/ssh/ssh_host_ed25519_key"];
-      sshUser = "root";
     };
     io = {
       hostname = "io";
@@ -16,34 +14,18 @@ inputs: {
         path = activate.nixos inputs.self.nixosConfigurations.io;
       };
     };
-    rog = {
-      hostname = "rog";
-      profiles.system = {
-        user = "root";
-        path = activate.nixos inputs.self.nixosConfigurations.rog;
-      };
-    };
     homesv = {
       hostname = "homesv";
       profiles.system = {
         user = "root";
         path = activate.nixos inputs.self.nixosConfigurations.homesv;
       };
-      sshOpts = ["-i" "/etc/ssh/ssh_host_ed25519_key"];
-      sshUser = "root";
     };
     kiiro = {
       hostname = "kiiro";
       profiles.system = {
         user = "root";
         path = activate.nixos inputs.self.nixosConfigurations.kiiro;
-      };
-    };
-    tosh = {
-      hostname = "tosh";
-      profiles.system = {
-        user = "root";
-        path = activate.nixos inputs.self.nixosConfigurations.tosh;
       };
     };
   };
