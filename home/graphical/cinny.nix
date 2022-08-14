b5f18c706f737781b7f7717f390dafe385362145
Mihai Fufezan <fufexan@protonmail.com>
2022-08-14 20:49:15 +0300

    home/graphical: add Cinny service

---
+++ home/graphical/cinny.nix
@@ -0,0 +1,13 @@
+{pkgs, ...}: {
+  systemd.user.services.cinny = {
+    Unit.Description = "Cinny Service";
+
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.darkhttpd}/bin/darkhttpd ${pkgs.cinny} --addr 127.0.0.1 --port 9999";
+      TimeoutStopSec = 5;
+    };
+
+    Install.WantedBy = ["default.target"];
+  };
+}
--- home/graphical/default.nix
+++ home/graphical/default.nix
@@ -8,6 +8,7 @@
 # includes programs and services that work on both Wayland and X
 {
   imports = [
+    ./cinny.nix
     ./discord
     ./files
     ./media.nix
