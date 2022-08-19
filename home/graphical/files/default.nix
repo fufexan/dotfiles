ccc435ec28452e2fbd4f6a1bf239b22f30fb986d
Mihai Fufezan <fufexan@protonmail.com>
2022-08-19 20:25:14 +0300

    home/graphical/files: update wallpaper hash

--- home/graphical/files/default.nix
+++ home/graphical/files/default.nix
@@ -9,7 +9,7 @@
     "wallpaper.jpg".source = builtins.fetchurl {
       name = "WavyLines02.jpg";
       url = "https://raw.githubusercontent.com/catppuccin/wallpapers/main/waves/wavy_lines_v02_5120x2880.png";
-      sha256 = "1yha3fbr7if2pmq9ylrcshnf8cmcz1r9whmr3b95kdgan0mm0pbz";
+      sha256 = "0b5bi27kjsiwjj4d4pcjnmm9ysscnhgwyd0qm40jsypp7zh78fb9";
     };
   };
 }
