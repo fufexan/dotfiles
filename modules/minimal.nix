8c92f161731aa38259986b2fb86c60aa5c0e9551
Mihai Fufezan <fufexan@protonmail.com>
2022-08-21 13:51:12 +0300

    add personal cachix to substituters

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -92,10 +92,12 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://helix.cachix.org"
+        "https://fufexan.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+        "fufexan.cachix.org-1:LwCDjCJNJQf5XD2BV+yamQIMZfcKWR9ISIFy5curUsY="
       ];
     };
   };
