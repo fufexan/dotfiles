6d86091ac8d8050088660b5b30c48fa241b235bc
Mihai Fufezan <fufexan@protonmail.com>
2022-08-25 21:36:19 +0300

    hyprland & sway: fix xwl scaling

--- pkgs/hyprland/default.nix
+++ pkgs/hyprland/default.nix
@@ -8,6 +8,11 @@ inputs: prev: let
           url = "https://gitlab.freedesktop.org/lilydjwg/wlroots/-/commit/6c5ffcd1fee9e44780a6a8792f74ecfbe24a1ca7.diff";
           sha256 = "sha256-Eo1pTa/PIiJsRZwIUnHGTIFFIedzODVf0ZeuXb0a3TQ=";
         })
+        (prev.fetchpatch {
+          url = "https://gitlab.freedesktop.org/wlroots/wlroots/-/commit/18595000f3a21502fd60bf213122859cc348f9af.diff";
+          sha256 = "sha256-jvfkAMh3gzkfuoRhB4E9T5X1Hu62wgUjj4tZkJm0mrI=";
+          revert = true;
+        })
       ];
     });
     inherit xwayland;
--- pkgs/sway-hidpi.nix
+++ pkgs/sway-hidpi.nix
@@ -32,6 +32,11 @@ prev: let
           url = "https://gitlab.freedesktop.org/lilydjwg/wlroots/-/commit/6c5ffcd1fee9e44780a6a8792f74ecfbe24a1ca7.diff";
           sha256 = "sha256-Eo1pTa/PIiJsRZwIUnHGTIFFIedzODVf0ZeuXb0a3TQ=";
         })
+        (prev.fetchpatch {
+          url = "https://gitlab.freedesktop.org/wlroots/wlroots/-/commit/18595000f3a21502fd60bf213122859cc348f9af.diff";
+          sha256 = "sha256-jvfkAMh3gzkfuoRhB4E9T5X1Hu62wgUjj4tZkJm0mrI=";
+          revert = true;
+        })
       ];
     }))
     .override {inherit xwayland;};
