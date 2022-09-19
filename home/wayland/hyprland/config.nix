22d492f66a88435c04cf242f32cc337989efc503
Mihai Fufezan <fufexan@protonmail.com>
2022-09-19 18:59:49 +0300

    home/graphical/eww: make bar edge-to-edge
    home/wayland/hyprland/config: enable smart gaps, small tweaks

--- home/wayland/hyprland/config.nix
+++ home/wayland/hyprland/config.nix
@@ -8,7 +8,6 @@
 
   emoji = "${pkgs.wofi-emoji}/bin/wofi-emoji";
   launcher = "wofi";
-  term = default.terminal.name;
 
   run-as-service = slice:
     pkgs.writeShellScript "as-systemd-transient" ''
@@ -19,161 +18,170 @@
         --wait \
         bash -lc "exec apply-hm-env $@"
     '';
+in ''
+  # should be configured per-profile
+  monitor=DP-1,preferred,auto,1
+  monitor=DP-2,preferred,auto,1
+  monitor=eDP-1,preferred,auto,1.6
+  workspace=eDP-1,1
+  workspace=DP-1,10
+  workspace=DP-2,10
 
-  config = ''
-    # should be configured per-profile
-    monitor=DP-1,preferred,auto,1
-    monitor=DP-2,preferred,auto,1
-    monitor=eDP-1,preferred,auto,1.6
-    workspace=eDP-1,1
-    workspace=DP-1,10
-    workspace=DP-2,10
+  exec-once=xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 2
+  exec-once=swaybg -i ~/.config/wallpaper.png
+  exec-once=swayidle timeout 300 "hyprctl dispatch dpms off" resume "hyprctl dispatch dpms on" timeout 310 "loginctl lock-session" timeout 600 "systemctl suspend" before-sleep swayidle lock swayidle
+  exec-once=eww daemon
+  exec-once=eww open bar
 
-    exec-once=xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 2
-    exec-once=swaybg -i ~/.config/wallpaper.png
-    exec-once=swayidle timeout 300 "hyprctl dispatch dpms off" resume "hyprctl dispatch dpms on" timeout 310 "loginctl lock-session" timeout 600 "systemctl suspend" before-sleep gtklock lock gtklock
-    exec-once=eww daemon
-    exec-once=eww open bar
+  misc {
+    no_vfr=0
+  }
 
-    misc {
-      no_vfr=0
-    }
+  gestures {
+    workspace_swipe=1
+  }
 
-    gestures {
-      workspace_swipe=1
-    }
+  input {
+    kb_layout=ro
 
-    input {
-      kb_layout=ro
+    follow_mouse=1
+    force_no_accel=1
+  }
 
-      follow_mouse=1
-      force_no_accel=1
-    }
+  device:MSFT0001:00 04F3:31EB Touchpad {
+    # sensitivity=-1
+    natural_scroll=1
+  }
 
-    device:MSFT0001:00 04F3:31EB Touchpad {
-      # sensitivity=-1
-      natural_scroll=1
-    }
+  general {
+    main_mod=SUPER
 
-    general {
-      main_mod=SUPER
+    gaps_in=5
+    gaps_out=5
+    border_size=2
+    col.active_border=0x${xargb.base06} # white
+    col.inactive_border=0x${xargb.base02} # black
+  }
 
-      gaps_in=5
-      gaps_out=5
-      border_size=2
-      col.active_border=0x${xargb.base06}
-      col.inactive_border=0x${xargb.base02}
-    }
+  decoration {
+    rounding=16
+    blur=1
+    blur_size=3
+    blur_passes=3
+    blur_new_optimizations=1
 
-    decoration {
-      rounding=16
-      blur=1
-      blur_size=3
-      blur_passes=3
-      blur_new_optimizations=1
+    drop_shadow=1
+    shadow_ignore_window=1
+    shadow_offset=2 2
+    shadow_range=4
+    shadow_render_power=1
+    col.shadow=0x55000000
+  }
 
-      drop_shadow=1
-      shadow_ignore_window=1
-      shadow_offset=2 2
-      shadow_range=4
-      shadow_render_power=1
-      col.shadow=0x55000000
-    }
+  animations {
+    enabled=1
+    animation=border,1,2,default
+    animation=fade,1,4,default
+    animation=windows,1,3,default,popin 80%
+    animation=workspaces,1,2,default,slide
+  }
 
-    animations {
-      enabled=1
-      animation=border,1,2,default
-      animation=fade,1,4,default
-      animation=windows,1,3,default,popin 80%
-      animation=workspaces,1,2,default,slide
-    }
+  dwindle {
+    pseudotile=1
+    preserve_split=1
+    no_gaps_when_only=1
+  }
 
-    dwindle {
-      pseudotile=1
-      preserve_split=1
-    }
+  windowrule=float,title:^(Media viewer)$
+  windowrule=float,title:^(Picture-in-Picture)$
+  windowrule=float,title:^(Firefox — Sharing Indicator)$
+  windowrule=move 0 0,title:^(Firefox — Sharing Indicator)$
 
-    windowrule=float,title:^(Media viewer)$
-    windowrule=float,title:^(Picture-in-Picture)$
-    windowrule=float,title:^(Firefox — Sharing Indicator)$
-    windowrule=move 0 0,title:^(Firefox — Sharing Indicator)$
+  windowrule=tile,title:^(Spotify)$
+  windowrule=workspace 9,title:^(Spotify)$
+  windowrule=workspace 2,title:^(Discord)$
 
-    windowrule=tile,title:^(Spotify)$
-    windowrule=workspace 9,title:^(Spotify)$
-    windowrule=workspace 2,title:^(Discord)$
+  # compositor commands
+  bind=SUPERSHIFT,E,exec,pkill Hyprland
+  bind=SUPER,Q,killactive,
+  bind=SUPER,F,fullscreen,
+  bind=SUPER,G,togglegroup,
+  bind=SUPERSHIFT,N,changegroupactive,f
+  bind=SUPERSHIFT,P,changegroupactive,b
+  bind=SUPER,R,togglesplit,
+  bind=SUPER,T,togglefloating,
+  bind=SUPER,P,pseudo,
+  bind=SUPERALT,,resizeactive,
 
-    # compositor commands
-    bind=SUPERSHIFT,E,exec,pkill Hyprland
-    bind=SUPER,Q,killactive,
-    bind=SUPER,F,fullscreen,
-    bind=SUPER,G,togglegroup,
-    bind=SUPERSHIFT,N,changegroupactive,f
-    bind=SUPERSHIFT,P,changegroupactive,b
-    bind=SUPER,R,togglesplit,
-    bind=SUPER,T,togglefloating,
-    bind=SUPER,P,pseudo,
-    bind=SUPERALT,,resizeactive,
+  # utility
+  bind=SUPER,Return,exec,${run-as-service "manual"} ${default.terminal.name}
+  bind=SUPER,Space,exec,${run-as-service "manual"} ${launcher}
+  bind=SUPER,Escape,exec,wlogout -p layer-shell
+  bind=SUPER,L,exec,gtklock
+  bind=SUPER,E,exec,${emoji}
+  bind=SUPER,O,exec,wl-ocr
+  # move focus
+  bind=SUPER,left,movefocus,l
+  bind=SUPER,right,movefocus,r
+  bind=SUPER,up,movefocus,u
+  bind=SUPER,down,movefocus,d
 
-    # utility
-    bind=SUPER,Return,exec,${run-as-service "manual"} ${term}
-    bind=SUPER,Space,exec,${run-as-service "manual"} ${launcher}
-    bind=SUPER,Escape,exec,wlogout -p layer-shell
-    bind=SUPER,L,exec,gtklock
-    bind=SUPER,E,exec,${emoji}
-    bind=SUPER,O,exec,wl-ocr
-    # move focus
-    bind=SUPER,left,movefocus,l
-    bind=SUPER,right,movefocus,r
-    bind=SUPER,up,movefocus,u
-    bind=SUPER,down,movefocus,d
+  # window resize
+  bind=SUPER,S,submap,resize
 
-    # media controls
-    bind=,XF86AudioPlay,exec,playerctl play-pause
-    bind=,XF86AudioPrev,exec,playerctl previous
-    bind=,XF86AudioNext,exec,playerctl next
+  submap=resize
+  binde=,right,resizeactive,10 0
+  binde=,left,resizeactive,-10 0
+  binde=,up,resizeactive,0 -10
+  binde=,down,resizeactive,0 10
+  bind=,escape,submap,reset
+  submap=reset
 
-    # volume
-    bindle=,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 6%+
-    bindle=,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 6%-
-    bind=,XF86AudioMute,exec,wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
-    bind=,XF86AudioMicMute,exec,wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+  # media controls
+  bind=,XF86AudioPlay,exec,playerctl play-pause
+  bind=,XF86AudioPrev,exec,playerctl previous
+  bind=,XF86AudioNext,exec,playerctl next
 
-    # backlight
-    bindle=,XF86MonBrightnessUp,exec,light -A 5
-    bindle=,XF86MonBrightnessDown,exec,light -U 5
+  # volume
+  bindle=,XF86AudioRaiseVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 6%+
+  bindle=,XF86AudioLowerVolume,exec,wpctl set-volume @DEFAULT_AUDIO_SINK@ 6%-
+  bind=,XF86AudioMute,exec,wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+  bind=,XF86AudioMicMute,exec,wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 
-    # screenshot
-    bind=,Print,exec,grimblast --notify copysave area
-    bind=SUPERSHIFT,R,exec,grimblast --notify copysave area
-    bind=CTRL,Print,exec,grimblast --notify --cursor copysave output
-    bind=SUPERSHIFTCTRL,R,exec,grimblast --notify --cursor copysave output
-    bind=ALT,Print,exec,grimblast --notify --cursor copysave screen
-    bind=SUPERSHIFTALT,R,exec,grimblast --notify --cursor copysave screen
+  # backlight
+  bindle=,XF86MonBrightnessUp,exec,light -A 5
+  bindle=,XF86MonBrightnessDown,exec,light -U 5
 
-    # workspaces
-    ${builtins.concatStringsSep "\n" (builtins.genList (
-        x: let
-          ws = let
-            c = (x + 1) / 10;
-          in
-            builtins.toString (x + 1 - (c * 10));
-        in ''
-          bind=SUPER,${ws},workspace,${toString (x + 1)}
-          bind=SHIFTSUPER,${ws},movetoworkspacesilent,${toString (x + 1)}
-        ''
-      )
-      10)}
+  # screenshot
+  bind=,Print,exec,grimblast --notify copysave area
+  bind=SUPERSHIFT,R,exec,grimblast --notify copysave area
+  bind=CTRL,Print,exec,grimblast --notify --cursor copysave output
+  bind=SUPERSHIFTCTRL,R,exec,grimblast --notify --cursor copysave output
+  bind=ALT,Print,exec,grimblast --notify --cursor copysave screen
+  bind=SUPERSHIFTALT,R,exec,grimblast --notify --cursor copysave screen
 
-    # special workspace
-    bind=SUPERSHIFT,asciitilde,movetoworkspace,special
-    bind=SUPER,grave,togglespecialworkspace,eDP-1
+  # workspaces
+  ${builtins.concatStringsSep "\n" (builtins.genList (
+      x: let
+        ws = let
+          c = (x + 1) / 10;
+        in
+          builtins.toString (x + 1 - (c * 10));
+      in ''
+        bind=SUPER,${ws},workspace,${toString (x + 1)}
+        bind=SHIFTSUPER,${ws},movetoworkspacesilent,${toString (x + 1)}
+      ''
+    )
+    10)}
 
-    # cycle workspaces
-    bind=SUPER,bracketleft,workspace,m-1
-    bind=SUPER,bracketright,workspace,m+1
-    # cycle monitors
-    bind=SUPERSHIFT,braceleft,focusmonitor,l
-    bind=SUPERSHIFT,braceright,focusmonitor,r
-  '';
-in
-  config
+  # special workspace
+  bind=SUPERSHIFT,asciitilde,movetoworkspace,special
+  bind=SUPER,grave,togglespecialworkspace,eDP-1
+
+  # cycle workspaces
+  bind=SUPER,bracketleft,workspace,m-1
+  bind=SUPER,bracketright,workspace,m+1
+  # cycle monitors
+  bind=SUPERSHIFT,braceleft,focusmonitor,l
+  bind=SUPERSHIFT,braceright,focusmonitor,r
+''
