eaf298b65465cab207254c56a1b866548594ba54
Mihai Fufezan <fufexan@protonmail.com>
2022-09-20 23:41:35 +0300

    home/files, modules/greetd: change wallpaper

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -7,8 +7,8 @@
 }: let
   wall = builtins.fetchurl rec {
     name = "wallpaper-${sha256}.png";
-    url = "https://raw.githubusercontent.com/catppuccin/wallpapers/main/landscapes/yosemite.png";
-    sha256 = "sha256-k1kikLv9wiubFhemFiW5NN4pMqMmHHhSwqs3AzeD138=";
+    url = "https://9to5mac.com/wp-content/uploads/sites/6/2014/08/yosemite-4.jpg?quality100&strip=all";
+    sha256 = "1hvpphvrdnlrdij2armq5zpi5djg2wj7hxhg148cgm9fs9m3z1vq";
   };
 
   schema = pkgs.gsettings-desktop-schemas;
