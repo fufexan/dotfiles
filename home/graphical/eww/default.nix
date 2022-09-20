1a7677c40aa00dffc83e2578587abfcad63387cf
Mihai Fufezan <fufexan@protonmail.com>
2022-09-20 19:38:55 +0300

    home/graphical/eww: fix systemd service

--- home/graphical/eww/default.nix
+++ home/graphical/eww/default.nix
@@ -8,6 +8,7 @@
   dependencies = with pkgs; [
     config.wayland.windowManager.hyprland.package
     config.programs.eww.package
+    bash
     bc
     bluez
     coreutils
@@ -28,26 +29,32 @@
     wofi
   ];
 in {
-  home.packages = dependencies;
-
   programs.eww = {
     enable = true;
     package = inputs.eww.packages.${pkgs.system}.eww-wayland;
-    configDir = ./.;
+
+    # remove nix files
+    configDir = lib.cleanSourceWith {
+      filter = name: type: let
+        baseName = baseNameOf (toString name);
+      in
+        !(lib.hasSuffix ".nix" baseName);
+      src = lib.cleanSource ./.;
+    };
   };
 
-  # systemd.user.services.eww = {
-  #   Unit = {
-  #     Description = "Eww Daemon";
-  #     # not yet implemented
-  #     # PartOf = ["tray.target"];
-  #     PartOf = ["graphical-session.target"];
-  #   };
-  #   Service = {
-  #     Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath dependencies}";
-  #     ExecStart = "${config.programs.eww.package}/bin/eww daemon --no-daemonize";
-  #     Restart = "on-failure";
-  #   };
-  #   Install.WantedBy = ["graphical-session.target"];
-  # };
+  systemd.user.services.eww = {
+    Unit = {
+      Description = "Eww Daemon";
+      # not yet implemented
+      # PartOf = ["tray.target"];
+      PartOf = ["graphical-session.target"];
+    };
+    Service = {
+      Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath dependencies}";
+      ExecStart = "${config.programs.eww.package}/bin/eww daemon --no-daemonize";
+      Restart = "on-failure";
+    };
+    Install.WantedBy = ["graphical-session.target"];
+  };
 }
