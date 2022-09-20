0e3b6a4a070778d4568c248532ac6c20c7a835f9
Mihai Fufezan <fufexan@protonmail.com>
2022-09-21 00:31:11 +0300

    hosts/arm-server: use minecraft from pkgs

--- hosts/servers/arm-server/minecraft.nix
+++ hosts/servers/arm-server/minecraft.nix
@@ -1,18 +1,7 @@
-{pkgs, ...}: let
-  nixpkgs-new =
-    import (pkgs.fetchFromGitHub {
-      owner = "NixOS";
-      repo = "nixpkgs";
-      rev = "195d38508ce4dfb302e815a4a3162c0883122213";
-      sha256 = "sha256-Ti6fTJ+P7SqcT+DrVzsbpxyjf+cr2LLcRVRi5v0a8bE=";
-    }) {
-      inherit (pkgs) system;
-      config.allowUnfree = true;
-    };
-in {
+{pkgs, ...}: {
   services.minecraft-server = {
     enable = true;
-    package = nixpkgs-new.minecraftServers.vanilla-1-19;
+    package = pkgs.minecraftServers.vanilla-1-19;
     eula = true;
     jvmOpts = "-Xms4092M -Xmx4092M
       -XX:+UseG1GC -XX:+CMSIncrementalPacing
