0d1f43b0de5fec9a112305985a0e17df9f94acd7
Mihai Fufezan <fufexan@protonmail.com>
2022-09-21 22:45:46 +0300

    modules: disable NM-wait-online in minimal

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -140,13 +140,6 @@
     rtkit.enable = true;
   };
 
-  # Don't wait for network startup
-  # https://old.reddit.com/r/NixOS/comments/vdz86j/how_to_remove_boot_dependency_on_network_for_a
-  systemd = {
-    targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
-    services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
-  };
-
   xdg.portal.enable = true;
   # wlroots screensharing
   xdg.portal.wlr = {
--- modules/minimal.nix
+++ modules/minimal.nix
@@ -136,6 +136,13 @@
 
   system.stateVersion = "20.09";
 
+  # Don't wait for network startup
+  # https://old.reddit.com/r/NixOS/comments/vdz86j/how_to_remove_boot_dependency_on_network_for_a
+  systemd = {
+    targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
+    services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
+  };
+
   time.timeZone = "Europe/Bucharest";
 
   users.users.mihai = {
