5e2016f8fe81faef72316dbb2fa06384c5df2697
Mihai Fufezan <fufexan@protonmail.com>
2022-09-27 20:00:41 +0300

    revert using default fonts

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -21,7 +21,17 @@
     ];
 
     # use fonts specified by user rather than default ones
-    enableDefaultFonts = true;
+    enableDefaultFonts = false;
+
+    # user defined fonts
+    # the reason there's Noto Color Emoji everywhere is to override DejaVu's
+    # B&W emojis that would sometimes show instead of some Color emojis
+    fontconfig.defaultFonts = {
+      serif = ["Noto Serif" "Noto Color Emoji"];
+      sansSerif = ["Noto Sans" "Noto Color Emoji"];
+      monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
+      emoji = ["Noto Color Emoji"];
+    };
   };
 
   environment.systemPackages = with pkgs; [glib];
