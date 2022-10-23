886ba9e310149880fa7e6f05ce5b0d2f48cae072
Mihai Fufezan <fufexan@protonmail.com>
2022-10-24 00:30:24 +0300

    discord-electron-openasar: update to 0.0.21

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,8 +2,14 @@ inputs: _: prev: rec {
   # instant repl with automatic flake loading
   repl = prev.callPackage ./repl {};
 
-  discord-electron-openasar = prev.callPackage ./discord {
-    inherit (prev.discord) src version pname;
+  discord-electron-openasar = prev.callPackage ./discord rec {
+    inherit (prev.discord) pname;
+    version = "0.0.21";
+    src = prev.fetchurl {
+      url = "https://dl.discordapp.net/apps/linux/${version}/discord-${version}.tar.gz";
+      sha256 = "sha256-KDKUssPRrs/D10s5GhJ23hctatQmyqd27xS9nU7iNaM=";
+    };
+
     openasar = prev.callPackage "${inputs.nixpkgs}/pkgs/applications/networking/instant-messengers/discord/openasar.nix" {};
     binaryName = "Discord";
     desktopName = "Discord";
