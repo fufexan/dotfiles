eb53854fcc3f42113e4e56373f8751c17ef5e40a
Mihai Fufezan <fufexan@protonmail.com>
2022-10-31 18:55:03 +0200

    flake.lock: update

--- home/graphical/spicetify.nix
+++ home/graphical/spicetify.nix
@@ -8,16 +8,6 @@
 
     spotifyPackage = inputs.self.packages.${pkgs.system}.spotify-wrapped-wm;
 
-    spicetifyPackage = pkgs.spicetify-cli.overrideAttrs (_: rec {
-      pname = "spicetify-cli";
-      version = "2.9.9";
-      src = pkgs.fetchgit {
-        url = "https://github.com/spicetify/${pname}";
-        rev = "v${version}";
-        sha256 = "1a6lqp6md9adxjxj4xpxj0j1b60yv3rpjshs91qx3q7blpsi3z4z";
-      };
-    });
-
     theme = "catppuccin-mocha";
     colorScheme = "flamingo";
 
