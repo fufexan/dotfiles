083bdde1f5dad194e84e9cec25c04e0b3f1a44ea
Mihai Fufezan <fufexan@protonmail.com>
2022-11-03 15:01:37 +0200

    home/graphical/files: move nix-index-update-db

--- home/graphical/files/default.nix
+++ home/graphical/files/default.nix
@@ -20,6 +20,7 @@
     };
   };
 
+  # Product Sans font
   home.file."${config.xdg.dataHome}/fonts/ProductSans".source = lib.cleanSourceWith {
     filter = name: _: (lib.hasSuffix ".ttf" (baseNameOf (toString name)));
     src = pkgs.fetchzip {
@@ -27,35 +28,5 @@
       sha256 = "sha256-PF2n4d9+t1vscpCRWZ0CR3X0XBefzL9BAkLHoqWFZR4=";
       stripRoot = false;
     };
- };
-
-  # set up nix-index
-  systemd.user.timers.nix-index-db-update = {
-    Timer = {
-      OnCalendar = "weekly";
-      Persistent = true;
-      RandomizedDelaySec = 0;
-    };
-  };
-
-  systemd.user.services.nix-index-db-update = {
-    Unit = {
-      Description = "nix-index database update";
-      PartOf = ["multi-user.target"];
-    };
-    Service = let
-      script = pkgs.writeShellScript "nix-index-update-db" ''
-        export filename="index-x86_64-$(uname | tr A-Z a-z)"
-        mkdir -p ~/.cache/nix-index
-        cd ~/.cache/nix-index
-        # -N will only download a new version if there is an update.
-        wget -N https://github.com/Mic92/nix-index-database/releases/latest/download/$filename -O $filename
-        ln -f $filename files
-      '';
-    in {
-      Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath [pkgs.wget pkgs.coreutils]}";
-      ExecStart = "${script}";
-    };
-    Install.WantedBy = ["multi-user.target"];
   };
 }
