e6d59d509c9de847d7ba3c5457fd0ae2a25281e2
Mihai Fufezan <fufexan@protonmail.com>
2022-11-30 14:04:28 +0200

    home/shell [bat]: add catppuccin theme

--- home/shell/cli.nix
+++ home/shell/cli.nix
@@ -17,7 +17,16 @@
       enable = true;
       config = {
         pager = "less -FR";
-        theme = "ansi";
+        theme = "Catppuccin-mocha";
+      };
+      themes = {
+        Catppuccin-mocha = builtins.readFile (pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "00bd462e8fab5f74490335dcf881ebe7784d23fa";
+            sha256 = "yzn+1IXxQaKcCK7fBdjtVohns0kbN+gcqbWVE4Bx7G8=";
+          }
+          + "/Catppuccin-mocha.tmTheme");
       };
     };
     bottom.enable = true;
