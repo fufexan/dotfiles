04454e80c3942f7bd884e2313170b454598507fd
Mihai Fufezan <fufexan@protonmail.com>
2023-02-10 20:42:05 +0200

    modules/greetd: reorganize module

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -5,14 +5,52 @@
   ...
 }:
 # greetd display manager
-{
+let
+  gtkgreetStyle = with default.xcolors;
+    pkgs.writeText "greetd-gtkgreet.css" ''
+      window {
+        background-color: black;
+        background-image: url("${default.wallpaper}");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+
+      #body > box > box > label, #clock {
+        color: ${text};
+        text-shadow: 0 0 3px ${crust};
+      }
+
+      entry {
+        background-color: ${base};
+        border-radius: 16px;
+        box-shadow: 0 0 5px ${crust};
+        color: ${text};
+      }
+
+      .text-button { border-radius: 16px; }
+    '';
+
+  greetdSwayConfig = pkgs.writeText "greetd-sway-config" ''
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -s ${gtkgreetStyle}; swaymsg exit"
+    bindsym Mod4+shift+e exec swaynag \
+      -t warning \
+      -m 'What do you want to do?' \
+      -b 'Poweroff' 'systemctl poweroff' \
+      -b 'Reboot' 'systemctl reboot'
+    seat seat0 xcursor_theme Bibata-Modern-Classic 24
+    exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP
+  '';
+in {
   environment.systemPackages = with pkgs; [
     # theme packages
-    (catppuccin-gtk.override {size = "compact";})
+    (catppuccin-gtk.override {
+      accents = ["mauve"];
+      size = "compact";
+      variant = "mocha";
+    })
     bibata-cursors
     papirus-icon-theme
-
-    greetd.gtkgreet
   ];
 
   # set gtk theme
@@ -22,54 +60,12 @@
     gtk-cursor-theme-size=24
     gtk-font-name=Roboto
     gtk-icon-theme-name=Papirus-Dark
-    gtk-theme-name=Catppuccin-Orange-Dark-Compact
+    gtk-theme-name=Catppuccin-Mocha-Compact-Mauve-Dark
   '';
 
   services.greetd = {
     enable = true;
-    settings = {
-      default_session.command = let
-        gtkgreetStyle = pkgs.writeText "greetd-gtkgreet.css" ''
-          window {
-            background-image: url("${default.wallpaper}");
-            background-position: center;
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-color: black;
-          }
-
-          #body > box > box > label {
-            text-shadow: 0 0 3px #1e1e2e;
-            color: #f5e0dc;
-          }
-
-          entry {
-            color: #f5e0dc;
-            background: rgba(30, 30, 46, 0.8);
-            border-radius: 16px;
-            box-shadow: 0 0 5px #1e1e2e;
-          }
-
-          #clock {
-            color: #f5e0dc;
-            text-shadow: 0 0 3px #1e1e2e;
-          }
-
-          .text-button { border-radius: 16px; }
-        '';
-        greetdSwayConfig = pkgs.writeText "greetd-sway-config" ''
-          exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -s ${gtkgreetStyle}; swaymsg exit"
-          bindsym Mod4+shift+e exec swaynag \
-            -t warning \
-            -m 'What do you want to do?' \
-            -b 'Poweroff' 'systemctl poweroff' \
-            -b 'Reboot' 'systemctl reboot'
-          seat seat0 xcursor_theme Bibata-Modern-Classic 24
-          exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP
-        '';
-      in "${inputs.self.packages.${pkgs.hostPlatform.system}.sway-hidpi}/bin/sway --config ${greetdSwayConfig}";
-      # in "${pkgs.dbus}/bin/dbus-run-session ${pkgs.cage}/bin/cage -s -- ${pkgs.greetd.gtkgreet} -l -s ${gtkgreetStyle}";
-    };
+    settings.default_session.command = "${inputs.self.packages.${pkgs.hostPlatform.system}.sway-hidpi}/bin/sway --config ${greetdSwayConfig}";
   };
 
   # selectable options
