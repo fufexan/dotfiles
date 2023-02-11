4a426a9d053faeb8a253cdbe267bc62ddc09e8e2
Mihai Fufezan <fufexan@protonmail.com>
2023-02-11 23:24:42 +0200

    modules/greetd: rework

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -14,6 +14,7 @@ let
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
+        font-family: "Jost *", Roboto, sans-serif;
       }
 
       #body > box > box > label, #clock {
@@ -31,15 +32,35 @@ let
       .text-button { border-radius: 16px; }
     '';
 
+  set_theme = let
+    gds = pkgs.gsettings-desktop-schemas;
+    gtk = pkgs.gtk3;
+  in
+    pkgs.writeShellScript "set_sway_theme" ''
+      gnome_schema=org.gnome.desktop.interface
+      export XDG_DATA_DIRS="${gtk}/share/gsettings-schemas/${gtk.name}:${gds}/share/gsettings-schemas/${gds.name}"
+
+      gsettings set $gnome_schema gtk-theme 'Catppuccin-Mocha-Compact-Mauve-Dark'
+      gsettings set $gnome_schema icon-theme 'Papirus-Dark'
+      gsettings set $gnome_schema cursor-theme 'Bibata-Modern-Classic'
+      gsettings set $gnome_schema cursor-size '24'
+      gsettings set $gnome_schema font-name 'Roboto 11'
+    '';
+
   greetdSwayConfig = pkgs.writeText "greetd-sway-config" ''
-    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -s ${gtkgreetStyle}; swaymsg exit"
+    exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
+    exec "${set_theme}"
+    seat seat0 xcursor_theme Bibata-Modern-Classic 24
+
+    xwayland disable
+
     bindsym Mod4+shift+e exec swaynag \
       -t warning \
       -m 'What do you want to do?' \
       -b 'Poweroff' 'systemctl poweroff' \
       -b 'Reboot' 'systemctl reboot'
-    seat seat0 xcursor_theme Bibata-Modern-Classic 24
-    exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP
+
+    exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -s ${gtkgreetStyle}; swaymsg exit"
   '';
 in {
   environment.systemPackages = with pkgs; [
@@ -53,20 +74,13 @@ in {
     papirus-icon-theme
   ];
 
-  # set gtk theme
-  environment.etc."gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-cursor-theme-name=Bibata-Modern-Classic
-    gtk-cursor-theme-size=24
-    gtk-font-name=Roboto
-    gtk-icon-theme-name=Papirus-Dark
-    gtk-theme-name=Catppuccin-Mocha-Compact-Mauve-Dark
-  '';
-
   services.greetd = {
     enable = true;
     settings.default_session.command = "${inputs.self.packages.${pkgs.hostPlatform.system}.sway-hidpi}/bin/sway --config ${greetdSwayConfig}";
   };
+
+  # unlock GPG keyring on login
+  security.pam.services.greetd.gnupg.enable = true;
 
   # selectable options
   environment.etc."greetd/environments".text = ''
