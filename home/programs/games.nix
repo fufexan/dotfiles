d129a5620a4326e50e00f291cdc88c1a0fc498a7
Mihai Fufezan <fufexan@protonmail.com>
2023-03-21 01:19:36 +0200

    home/games: add lutris

--- home/programs/games.nix
+++ home/programs/games.nix
@@ -8,5 +8,7 @@
   home.packages = with pkgs; [
     inputs.nix-gaming.packages.${pkgs.hostPlatform.system}.osu-lazer-bin
     gamescope
+    (lutris.override {extraPkgs = p: [p.libnghttp2];})
+    winetricks
   ];
 }
