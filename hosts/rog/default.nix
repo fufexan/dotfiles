a6d2450d5ff98dfe64fbf218aabb6d980893d891
Mihai Fufezan <fufexan@protonmail.com>
2023-04-18 23:08:55 +0300

    flake.lock: update

--- hosts/rog/default.nix
+++ hosts/rog/default.nix
@@ -27,16 +27,6 @@
 
   boot.plymouth.enable = true;
 
-  environment.systemPackages = let
-    nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-      export __NV_PRIME_RENDER_OFFLOAD=1
-      export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-      export __GLX_VENDOR_LIBRARY_NAME=nvidia
-      export __VK_LAYER_NV_optimus=NVIDIA_only
-      exec -a "$0" "$@"
-    '';
-  in [nvidia-offload];
-
   hardware = {
     bluetooth = {
       enable = true;
@@ -68,7 +58,10 @@
     nvidia = {
       modesetting.enable = true;
       prime = {
-        offload.enable = true;
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
       };
