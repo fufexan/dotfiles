34bb0a1081be06ecb98b34eb906965e1e322e53b
Mihai Fufezan <fufexan@protonmail.com>
2023-05-08 01:57:46 +0300

    home/shell/starship: configure icons

--- home/shell/starship.nix
+++ home/shell/starship.nix
@@ -8,6 +8,18 @@
         success_symbol = "[›](bold green)";
         error_symbol = "[›](bold red)";
       };
+
+      git_status = {
+        deleted = "✗";
+        modified = "✶";
+        staged = "✓";
+        stashed = "≡";
+      };
+
+      nix_shell = {
+        symbol = " ";
+        heuristic = true;
+      };
     };
   };
 }
