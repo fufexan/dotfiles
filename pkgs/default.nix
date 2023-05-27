f3c6a72fe6a46d3c6ad3c1f7c3e971696f004e37
Mihai Fufezan <fufexan@protonmail.com>
2023-05-27 16:44:07 +0300

    pkgs: get rid of overlay

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,9 +5,31 @@
 }: {
   systems = ["x86_64-linux"];
 
-  flake.overlays.default = import ./overlays.nix;
-
   perSystem = {pkgs, ...}: {
-    packages = self.overlays.default inputs null pkgs;
+    packages = {
+      # instant repl with automatic flake loading
+      repl = pkgs.callPackage ./repl {};
+
+      catppuccin-plymouth = pkgs.callPackage ./catppuccin-plymouth {};
+
+      discord-canary = pkgs.discord-canary.override {
+        nss = pkgs.nss_latest;
+        withOpenASAR = true;
+      };
+
+      gdb-frontend = pkgs.callPackage ./gdb-frontend {};
+
+      howdy = pkgs.callPackage ./howdy {};
+
+      linux-enable-ir-emitter = pkgs.callPackage ./linux-enable-ir-emitter {};
+
+      waveform = pkgs.callPackage ./waveform {};
+
+      spotify = pkgs.callPackage ./spotify {
+        nss = pkgs.nss_latest;
+      };
+
+      sway-hidpi = import ./sway-hidpi.nix inputs pkgs;
+    };
   };
 }
