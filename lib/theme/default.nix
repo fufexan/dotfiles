4c14bacf2bb86723cae9acf501231a90ad7bdbfb
Mihai Fufezan <fufexan@protonmail.com>
2023-07-02 18:45:14 +0300

    eww/battery: use gross command

--- lib/theme/default.nix
+++ lib/theme/default.nix
@@ -22,6 +22,6 @@
   wallpaper = builtins.fetchurl rec {
     name = "wallpaper-${sha256}.png";
     url = "https://images.unsplash.com/photo-1529840882932-55f06ab2c681";
-    sha256 = "1rdkal6ry2g9i2i6aisg5j0a234m2sz1xyj3h8mdkmq981q90y5k";
+    sha256 = "1xngx610skv1vqzx1c7j2zv5cg3gld3hkcxki8jd30rssjjx98p2";
   };
 }
