c53dd0a43f448c68a5fa9b02c9d05d480a92b016
Mihai Fufezan <fufexan@protonmail.com>
2023-08-14 00:20:55 +0300

    wlogout: abstraction

--- home/programs/files/wlogout.nix
+++ home/programs/files/wlogout.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   bgImageSection = name: ''
     #${name} {
       background-image: image(url("${pkgs.wlogout}/share/wlogout/icons/${name}.png"));
@@ -7,7 +11,7 @@
 in {
   xdg.configFile."wlogout/style.css".text = ''
     * {
-    	background-image: none;
+      background-image: none;
       font-family: "Product Sans", Roboto, sans-serif;
     }
 
@@ -20,21 +24,23 @@ in {
       border-radius: 8px;
       box-shadow: inset 0 0 0 1px rgba(255, 255, 255, .1), 0 0 rgba(0, 0, 0, .4);
       margin: 1rem;
-    	background-repeat: no-repeat;
-    	background-position: center;
-    	background-size: 25%;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: 25%;
     }
 
     button:focus, button:active, button:hover {
       background-color: rgba(255, 255, 255, 0.2);
-    	outline-style: none;
+      outline-style: none;
     }
 
-    ${bgImageSection "lock"}
-    ${bgImageSection "logout"}
-    ${bgImageSection "suspend"}
-    ${bgImageSection "hibernate"}
-    ${bgImageSection "shutdown"}
-    ${bgImageSection "reboot"}
+    ${lib.concatMapStringsSep "\n" bgImageSection [
+      "lock"
+      "logout"
+      "suspend"
+      "hibernate"
+      "shutdown"
+      "reboot"
+    ]}
   '';
 }
