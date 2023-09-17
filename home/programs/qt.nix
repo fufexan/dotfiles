5dabb2f283f1f8cdc06b7526ca670f422edc0394
Mihai Fufezan <fufexan@protonmail.com>
2023-09-17 23:29:27 +0300

    home/programs/qt: use Adwaita Dark

--- home/programs/qt.nix
+++ home/programs/qt.nix
@@ -1,18 +1,8 @@
-{pkgs, ...}:
 # Qt theming with Kvantum
 {
-  home.packages = with pkgs; [
-    libsForQt5.qtstyleplugin-kvantum
-    (catppuccin-kvantum.override {
-      accent = "Mauve";
-      variant = "Mocha";
-    })
-  ];
-  home.sessionVariables = {
-    QT_STYLE_OVERRIDE = "kvantum";
-  };
-
-  xdg.configFile."Kvantum/kvantum.kvconfig".source = (pkgs.formats.ini {}).generate "kvantum.kvconfig" {
-    General.Theme = "Catppuccin-Mocha-Mauve";
+  qt = {
+    enable = true;
+    platformTheme = "gtk";
+    style.name = "Adwaita Dark";
   };
 }
