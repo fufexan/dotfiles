135909245d92a8d3427e4cd915208af47341985e
Mihai Fufezan <fufexan@protonmail.com>
2023-11-28 13:00:47 +0200

    lib/theme: change wallpaper size

--- lib/theme/default.nix
+++ lib/theme/default.nix
@@ -10,8 +10,9 @@ lib: {
   };
 
   wallpaper = let
-    url = "https://images.unsplash.com/photo-1690460550070-e73402127f11?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=marlon-medau-TFg35jn95OU-unsplash.jpg";
-    sha256 = "18cyaxq4bw6qd79yvjgq4nsxspy0jwq88dy00z92whm5d13ckvgm";
+    size = "&w=2560&h=1600";
+    url = "https://images.unsplash.com/photo-1690460550070-e73402127f11?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb&dl=marlon-medau-TFg35jn95OU-unsplash${size}.jpg";
+    sha256 = "0w2rp7km4r6wq8jihfyh36g76c0s1jcidpv4q2c2yl7mrs8gfj27";
     ext = lib.last (lib.splitString "." url);
   in
     builtins.fetchurl {
