3ade1db789299019008f5337130d97113eccd038
Mihai Fufezan <fufexan@protonmail.com>
2023-11-28 12:44:12 +0200

    ags: create service

--- home/programs/ags/default.nix
+++ home/programs/ags/default.nix
@@ -1,3 +1,37 @@
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  dependencies = with pkgs; [
+    config.wayland.windowManager.hyprland.package
+    bash
+    coreutils
+    gawk
+    inotify-tools
+    procps
+    ripgrep
+    sassc
+  ];
+
+  cfg = config.programs.ags;
+in {
   programs.ags.enable = true;
+
+  systemd.user.services.ags = {
+    Unit = {
+      Description = "Aylur's Gtk Shell";
+      PartOf = [
+        "tray.target"
+        "graphical-session.target"
+      ];
+    };
+    Service = {
+      Environment = "PATH=/run/wrappers/bin:${lib.makeBinPath dependencies}";
+      ExecStart = "${cfg.package}/bin/ags";
+      Restart = "on-failure";
+    };
+    Install.WantedBy = ["graphical-session.target"];
+  };
 }
