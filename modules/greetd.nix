299d04db851437ab45721e839a5b81c129351b02
Mihai Fufezan <fufexan@protonmail.com>
2023-12-07 00:04:06 +0200

    modules/greetd: use cage instead of sway

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   pkgs,
   config,
   default,
@@ -7,23 +6,6 @@
 }:
 # greetd display manager
 let
-  greetdSwayConfig = pkgs.writeText "greetd-sway-config" ''
-    exec "dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK XDG_CURRENT_DESKTOP"
-    input "type:touchpad" {
-      tap enabled
-    }
-    seat seat0 xcursor_theme Bibata-Modern-Classic 24
-    xwayland disable
-
-    bindsym Mod4+shift+e exec swaynag \
-      -t warning \
-      -m 'What do you want to do?' \
-      -b 'Poweroff' 'systemctl poweroff' \
-      -b 'Reboot' 'systemctl reboot'
-
-    exec "${lib.getExe config.programs.regreet.package} -l debug; swaymsg exit"
-  '';
-
   inherit (config.programs.matugen) variant;
 
   themeName =
@@ -49,22 +31,8 @@ in {
 
   programs.regreet = {
     enable = true;
-    package = pkgs.greetd.regreet.overrideAttrs (self: super: {
-      version = "0.1.1-patched";
-      src = pkgs.fetchFromGitHub {
-        owner = "rharish101";
-        repo = "ReGreet";
-        rev = "61d871a0ee5c74230dfef8100d0c9bc75b309203";
-        hash = "sha256-PkQTubSm/FN3FXs9vBB3FI4dXbQhv/7fS1rXkVsTAAs=";
-      };
-      cargoDeps = super.cargoDeps.overrideAttrs (_: {
-        inherit (self) src;
-        outputHash = "sha256-dR6veXCGVMr5TbCvP0EqyQKTG2XM65VHF9U2nRWyzfA=";
-      });
 
-      # temp fix until https://github.com/rharish101/ReGreet/issues/32 is solved
-      patches = [../pkgs/regreet.patch];
-    });
+    cageArgs = ["-s" "-m" "last"];
 
     settings = {
       background = {
@@ -79,12 +47,6 @@ in {
       };
     };
   };
-
-  programs.sway.enable = true;
-
-  services.greetd.settings.default_session.command =
-    "${config.programs.sway.package}/bin/sway --config ${greetdSwayConfig}"
-    + (lib.optionalString (config.networking.hostName == "rog") " --unsupported-gpu");
 
   # unlock GPG keyring on login
   security.pam.services.greetd.enableGnomeKeyring = true;
