95468115122220b94c757cb4ddb7593394d6e9e6
Mihai Fufezan <fufexan@protonmail.com>
2023-12-15 02:44:12 +0200

    modules/desktop: add hyprland portals config

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -214,6 +214,13 @@
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
-    config.common.default = ["gtk"];
+    config = {
+      common.default = ["gtk"];
+      hyprland.default = ["gtk" "hyprland"];
+    };
+
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
   };
 }
