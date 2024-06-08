162a69be633e7ab68b9e5a8e400a101750c0ebff
Mihai Fufezan <mihai@fufexan.net>
2024-06-08 16:27:17 +0300

    flake.lock: update
    network: use DNS over TLS

--- system/network/default.nix
+++ system/network/default.nix
@@ -1,10 +1,15 @@
 {lib, ...}:
 # networking configuration
 {
-  networking.networkmanager = {
-    enable = true;
-    dns = "systemd-resolved";
-    wifi.powersave = true;
+  networking = {
+    # use quad9 with DNS over TLS
+    nameservers = ["9.9.9.9#dns.quad9.net"];
+
+    networkmanager = {
+      enable = true;
+      dns = "systemd-resolved";
+      wifi.powersave = true;
+    };
   };
 
   services = {
@@ -14,9 +19,12 @@
     };
 
     # DNS resolver
-    resolved.enable = true;
+    resolved = {
+      enable = true;
+      dnsovertls = "opportunistic";
+    };
   };
 
   # Don't wait for network startup
-  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  # systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
 }
