1a1f8446ae0c4ed60591f4613be8c67d852eb094
Mihai Fufezan <mihai@fufexan.net>
2024-07-15 20:44:34 +0300

    hypridle: fix audio inhibiting suspend

--- home/services/wayland/hypridle.nix
+++ home/services/wayland/hypridle.nix
@@ -6,8 +6,9 @@
   ...
 }: let
   suspendScript = pkgs.writeShellScript "suspend-script" ''
-    ${pkgs.pipewire}/bin/pw-cli i all 2>&1 | ${pkgs.ripgrep}/bin/rg running -q
-    # only suspend if audio isn't running
+    # check if any player has status "Playing"
+    ${lib.getExe pkgs.playerctl} -a status | ${lib.getExe pkgs.ripgrep} running -q
+    # only suspend if nothing is playing
     if [ $? == 1 ]; then
       ${pkgs.systemd}/bin/systemctl suspend
     fi
