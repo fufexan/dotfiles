d513925a383415f30593c83f0d893165381c6f25
Mihai Fufezan <mihai@fufexan.net>
2024-07-29 19:08:23 +0300

    hypridle: fix rg query
    thanks @hmanhng

--- home/services/wayland/hypridle.nix
+++ home/services/wayland/hypridle.nix
@@ -7,7 +7,7 @@
 }: let
   suspendScript = pkgs.writeShellScript "suspend-script" ''
     # check if any player has status "Playing"
-    ${lib.getExe pkgs.playerctl} -a status | ${lib.getExe pkgs.ripgrep} running -q
+    ${lib.getExe pkgs.playerctl} -a status | ${lib.getExe pkgs.ripgrep} Playing -q
     # only suspend if nothing is playing
     if [ $? == 1 ]; then
       ${pkgs.systemd}/bin/systemctl suspend
