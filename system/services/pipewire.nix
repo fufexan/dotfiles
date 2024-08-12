0c15028154598d210f193601bcc1a670e8d09d1e
Mihai Fufezan <mihai@fufexan.net>
2024-08-12 18:15:19 +0300

    pipewire: disable camera in wireplumber
    https://www.reddit.com/r/linux/comments/1em8biv/psa_pipewire_has_been_halving_your_battery_life/

--- system/services/pipewire.nix
+++ system/services/pipewire.nix
@@ -5,6 +5,8 @@
     alsa.support32Bit = true;
     jack.enable = true;
     pulse.enable = true;
+
+    wireplumber.extraConfig."wireplumber.profiles".main."monitor.libcamera" = "disabled";
   };
 
   hardware.pulseaudio.enable = lib.mkForce false;
