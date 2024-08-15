a0bebadeb029ef50f2fa99ad3e7751a98b835610
Mihai Fufezan <mihai@fufexan.net>
2024-08-15 23:02:18 +0300

    home/programs/qt: use qtct + kvantum
    qtct is needed, otherwise apps don't find the proper icon theme/font settings.

--- home/programs/qt.nix
+++ home/programs/qt.nix
@@ -1,4 +1,9 @@
-{pkgs, ...}: let
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: let
   KvLibadwaita = pkgs.fetchFromGitHub {
     owner = "GabePoel";
     repo = "KvLibadwaita";
@@ -6,15 +11,32 @@
     hash = "sha256-K/2FYOtX0RzwdcGyeurLXAh3j8ohxMrH2OWldqVoLwo=";
     sparseCheckout = ["src"];
   };
+
+  qtctConf = {
+    Appearance = {
+      custom_palette = false;
+      icon_theme = config.gtk.iconTheme.name;
+      standard_dialogs = "xdgdesktopportal";
+      style = "kvantum";
+    };
+  };
+
+  defaultFont = "${config.gtk.font.name},${builtins.toString config.gtk.font.size}";
 in {
   qt = {
     enable = true;
-    style.name = "kvantum";
+    platformTheme = "qtct";
   };
 
-  home.packages = [pkgs.qt6Packages.qtstyleplugin-kvantum];
+  home.packages = [
+    pkgs.qt6Packages.qtstyleplugin-kvantum
+    pkgs.qt6Packages.qt6ct
+    pkgs.libsForQt5.qtstyleplugin-kvantum
+    pkgs.libsForQt5.qt5ct
+  ];
 
   xdg.configFile = {
+    # Kvantum config
     "Kvantum" = {
       source = "${KvLibadwaita}/src";
       recursive = true;
@@ -24,5 +46,32 @@ in {
       [General]
       theme=KvLibadwaitaDark
     '';
+
+    # qtct config
+    "qt5ct/qt5ct.conf".text = let
+      default = ''"${defaultFont},-1,5,50,0,0,0,0,0"'';
+    in
+      lib.generators.toINI {} (
+        qtctConf
+        // {
+          Fonts = {
+            fixed = default;
+            general = default;
+          };
+        }
+      );
+
+    "qt6ct/qt6ct.conf".text = let
+      default = ''"${defaultFont},-1,5,400,0,0,0,0,0,0,0,0,0,0,1,Regular"'';
+    in
+      lib.generators.toINI {} (
+        qtctConf
+        // {
+          Fonts = {
+            fixed = default;
+            general = default;
+          };
+        }
+      );
   };
 }
