5483d2e91882f8dfa2e033dfc07e064e5c656acb
zmazanv <95514881+zmazanv@users.noreply.github.com>
2024-09-11 08:59:07 -0400

    system/nix: filter out non-flake inputs from system flake registry (#43)

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -14,11 +14,13 @@
   # we need git for flakes
   environment.systemPackages = [pkgs.git];
 
-  nix = {
+  nix = let
+    flakeInputs = lib.filterAttrs (_: v: lib.isType "flake" v) inputs;
+  in {
     package = pkgs.lix;
 
     # pin the registry to avoid downloading and evaling a new nixpkgs version every time
-    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
+    registry = lib.mapAttrs (_: v: {flake = v;}) flakeInputs;
 
     # set the path for channels compat
     nixPath = lib.mapAttrsToList (key: _: "${key}=flake:${key}") config.nix.registry;
