14ebc62b5108e769deea85821abc9a4ac4ff2340
Mihai Fufezan <mihai@fufexan.net>
2024-09-12 23:23:57 +0300

    home/profiles/io: change touchpad acceleration curve

--- home/profiles/io/default.nix
+++ home/profiles/io/default.nix
@@ -38,7 +38,8 @@
   ];
 
   wayland.windowManager.hyprland.settings = let
-    accelpoints = "0.21 0.000 0.040 0.080 0.140 0.200 0.261 0.326 0.418 0.509 0.601 0.692 0.784 0.875 0.966 1.058 1.149 1.241 1.332 1.424 1.613";
+    # Generated using https://gist.github.com/fufexan/e6bcccb7787116b8f9c31160fc8bc543
+    accelpoints = "0.5 0.000 0.053 0.115 0.189 0.280 0.391 0.525 0.687 0.880 1.108 1.375 1.684 2.040 2.446 2.905 3.422 4.000 4.643 5.355 6.139";
   in {
     monitor = [
       "DP-1, preferred, auto-left, auto"
