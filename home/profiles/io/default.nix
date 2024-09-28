cd2b97d9ec73b970e73db5226398e278434e80b4
Mihai Fufezan <mihai@fufexan.net>
2024-09-28 19:39:40 +0300

    home/profiles/io: disable monitors on the left

--- home/profiles/io/default.nix
+++ home/profiles/io/default.nix
@@ -41,8 +41,8 @@
     accelpoints = "0.5 0.000 0.053 0.115 0.189 0.280 0.391 0.525 0.687 0.880 1.108 1.375 1.684 2.040 2.446 2.905 3.422 4.000 4.643 5.355 6.139";
   in {
     monitor = [
-      "DP-1, preferred, auto-left, auto"
-      "DP-2, preferred, auto-left, auto"
+      # "DP-1, preferred, auto-left, auto"
+      # "DP-2, preferred, auto-left, auto"
       "eDP-1, preferred, auto, 1.600000"
     ];
 
