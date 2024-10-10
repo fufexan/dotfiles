d4949ff327ea0c51fc3bd67d65f9c525c49d6427
Mihai Fufezan <mihai@fufexan.net>
2024-10-10 21:44:33 +0300

    xdg: fix zen

--- home/terminal/programs/xdg.nix
+++ home/terminal/programs/xdg.nix
@@ -3,7 +3,7 @@
   pkgs,
   ...
 }: let
-  browser = ["zen-browser"];
+  browser = ["zen"];
   imageViewer = ["org.gnome.Loupe"];
   videoPlayer = ["io.github.celluloid_player.Celluloid"];
   audioPlayer = ["io.bassi.Amberol"];
