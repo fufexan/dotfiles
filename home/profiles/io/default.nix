05d9933d3c49664e95fcfdd2b5d70eb4dd1e005c
Mihai Fufezan <mihai@fufexan.net>
2024-12-12 17:56:46 +0200

    home/profiles/io: disable wluma
    doesn't work how I expect it to and I don't know how to configure it to
    work properly.

--- home/profiles/io/default.nix
+++ home/profiles/io/default.nix
@@ -30,7 +30,7 @@
     ../../services/wayland/gammastep.nix
     ../../services/wayland/hyprpaper.nix
     ../../services/wayland/hypridle.nix
-    ../../services/wayland/wluma.nix
+    # ../../services/wayland/wluma.nix
 
     # terminal emulators
     ../../terminal/emulators/foot.nix
