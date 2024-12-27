324658a292696161fa6508d6575ef03cb2b82d29
Mihai Fufezan <mihai@fufexan.net>
2024-12-27 11:27:48 +0200

    before-sleep: use powerDownCommands option
    works better than my attempt

--- system/services/default.nix
+++ system/services/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   services = {
     dbus.implementation = "broker";
 
@@ -11,22 +11,14 @@
 
   # Use in place of hypridle's before_sleep_cmd, since systemd does not wait for
   # it to complete
-  systemd.services.before-sleep = let
-    suspendScript = pkgs.writeShellScript "suspend-script" ''
+  powerManagement = {
+    enable = true;
+    powerDownCommands = ''
       # Lock all sessions
       loginctl lock-sessions
 
       # Wait for lockscreen(s) to be up
-      sleep 3
+      sleep 1
     '';
-  in {
-    requiredBy = ["sleep.target"];
-    partOf = ["sleep.target"];
-    description = "Commands run before sleep";
-
-    serviceConfig = {
-      ExecStart = suspendScript.outPath;
-      Type = "oneshot";
-    };
   };
 }
