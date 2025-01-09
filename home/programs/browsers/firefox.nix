d887cc9ebb352502aae9577efb176cdc98098888
Mihai Fufezan <mihai@fufexan.net>
2025-01-09 14:52:01 +0200

    browsers/firefox: remove shyfox

--- home/programs/browsers/firefox.nix
+++ home/programs/browsers/firefox.nix
@@ -1,15 +1,4 @@
 {
-  config,
-  pkgs,
-  ...
-}: let
-  shyfox = pkgs.fetchFromGitHub {
-    owner = "Naezr";
-    repo = "ShyFox";
-    rev = "8d0d0139bbdb538a64e5a05df907160c39c8f008";
-    hash = "sha256-k3p8VxFpI/jw1TLBOKskH4KylsiiWBJLRNpffm+w7Bo=";
-  };
-in {
   programs.firefox = {
     enable = true;
     profiles.mihai = {
@@ -19,9 +8,6 @@ in {
         "general.autoScroll" = true;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
       };
-      extraConfig = builtins.readFile "${shyfox}/user.js";
     };
   };
-
-  home.file.".mozilla/firefox/${config.programs.firefox.profiles.mihai.path}/chrome".source = "${shyfox}/chrome";
 }
