8632528873f96f5abab89332a18bb7528b917a1a
Mihai Fufezan <mihai@fufexan.net>
2025-02-23 17:03:10 +0200

    games: use higher latency for osu!lazer
    Too low and it starts to crackle, and can also mess up rendering (?)

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -6,10 +6,12 @@
 # games
 {
   home.packages = with pkgs; [
-    inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin
+    # Default latency sometimes crackles
+    (inputs.nix-gaming.packages.${pkgs.system}.osu-lazer-bin.override {pipewire_latency = "128/48000";})
     gamescope
     prismlauncher
     # (lutris.override {extraPkgs = p: [p.libnghttp2];})
     winetricks
+    protontricks
   ];
 }
