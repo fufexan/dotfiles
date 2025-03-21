1f08552fc0842dd45287eff5805b814cf20f3c4d
Mihai Fufezan <mihai@fufexan.net>
2025-03-21 11:52:59 +0200

    flake.lock: update

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -27,35 +27,7 @@
     wineWowPackages.wayland
 
     inputs.nix-matlab.packages.${pkgs.system}.matlab
-    # FIXME: https://nixpk.gs/pr-tracker.html?pr=390243
-    (zotero.overrideAttrs (self: super: {
-      libPath =
-        lib.makeLibraryPath (with pkgs; [
-          alsa-lib
-          atk
-          cairo
-          dbus-glib
-          gdk-pixbuf
-          glib
-          gtk3
-          libGL
-          xorg.libX11
-          xorg.libXcomposite
-          xorg.libXcursor
-          xorg.libXdamage
-          xorg.libXext
-          xorg.libXfixes
-          xorg.libXi
-          xorg.libXrandr
-          xorg.libXtst
-          xorg.libxcb
-          libgbm
-          pango
-          pciutils
-        ])
-        + ":"
-        + lib.makeSearchPathOutput "lib" "lib" [stdenv.cc.cc];
-    }))
+    zotero
   ];
 
   xdg.configFile."matlab/nix.sh".text = "INSTALL_DIR=$XDG_DATA_HOME/matlab/installation";
--- home/programs/media/default.nix
+++ home/programs/media/default.nix
@@ -9,29 +9,8 @@
   home.packages = with pkgs; [
     # audio control
     pulsemixer
-    # fix build until https://github.com/NixOS/nixpkgs/pull/389740 lands in unstable
-    (pwvucontrol.overrideAttrs (self: super: let
-      wireplumber_0_4 = wireplumber.overrideAttrs (attrs: rec {
-        version = "0.4.17";
-
-        src = fetchFromGitLab {
-          domain = "gitlab.freedesktop.org";
-          owner = "pipewire";
-          repo = "wireplumber";
-          tag = version;
-          hash = "sha256-vhpQT67+849WV1SFthQdUeFnYe/okudTQJoL3y+wXwI=";
-        };
-
-        patches = [
-          (fetchpatch {
-            url = "https://gitlab.freedesktop.org/pipewire/wireplumber/-/commit/f4f495ee212c46611303dec9cd18996830d7f721.patch";
-            hash = "sha256-dxVlXFGyNvWKZBrZniFatPPnK+38pFGig7LGAsc6Ydc=";
-          })
-        ];
-      });
-    in {
-      buildInputs = (builtins.filter (e: e.pname != "wireplumber") super.buildInputs) ++ [wireplumber_0_4];
-    }))
+    pwvucontrol
+    helvum
 
     # audio
     amberol
