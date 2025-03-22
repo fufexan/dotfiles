abbfe63d74d42b4284b5ce3a8883041c6298357f
Mihai Fufezan <mihai@fufexan.net>
2025-03-22 14:05:34 +0200

    home/specialisations: change wallpaper

--- home/specialisations.nix
+++ home/specialisations.nix
@@ -1,9 +1,9 @@
 {
   theme = {
     wallpaper = let
-      url = "https://github.com/saint-13/Linux_Dynamic_Wallpapers/blob/main/Dynamic_Wallpapers/ChromeOSWind/ChromeOSWind-2.png?raw=true";
-      sha256 = "0j4m3azrwgfh3rahmasv4c8pr40x1brbn77nx54hpmg8pb9i67cc";
-      ext = "png";
+      url = "https://images.unsplash.com/photo-1529528744093-6f8abeee511d?ixlib=rb-4.0.3&q=85&fm=jpg&crop=fit&cs=srgb&width=2560.jpg";
+      sha256 = "00v40dxc0l266hmjcdp3c3yw7l8qmz46aw2z6nqj5dvw6vmd6xfj";
+      ext = "jpg";
     in
       builtins.fetchurl {
         name = "wallpaper-${sha256}.${ext}";
