6e3c0e5ee823c125f7d9bf2cee0e12cc107aaca1
Mihai Fufezan <mihai@fufexan.net>
2025-03-23 14:01:55 +0200

    home/specialisations: fix wallpaper res

--- home/specialisations.nix
+++ home/specialisations.nix
@@ -1,8 +1,8 @@
 {
   theme = {
     wallpaper = let
-      url = "https://images.unsplash.com/photo-1529528744093-6f8abeee511d?ixlib=rb-4.0.3&q=85&fm=jpg&crop=fit&cs=srgb&width=2560.jpg";
-      sha256 = "00v40dxc0l266hmjcdp3c3yw7l8qmz46aw2z6nqj5dvw6vmd6xfj";
+      url = "https://images.unsplash.com/photo-1529528744093-6f8abeee511d?ixlib=rb-4.0.3&q=85&fm=jpg&crop=fit&cs=srgb&w=2560";
+      sha256 = "18r5hmzglifysjmwn5j89gbbk56lbfb3f2jzwp432lr8gb5n7q8v";
       ext = "jpg";
     in
       builtins.fetchurl {
