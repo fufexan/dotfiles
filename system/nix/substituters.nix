8c9026be02965f7bbdf27ea320874d1436abcdb5
Mihai Fufezan <mihai@fufexan.net>
2025-12-25 21:02:33 +0200

    system/nix/substituters: add vicinae

--- system/nix/substituters.nix
+++ system/nix/substituters.nix
@@ -10,6 +10,7 @@
       "https://hyprland.cachix.org"
       "https://nix-community.cachix.org"
       "https://nix-gaming.cachix.org"
+      "https://vicinae.cachix.org"
       "https://yazi.cachix.org"
     ];
 
@@ -22,6 +23,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
       "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
     ];
   };
