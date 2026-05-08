c7e3f2acd299c377cc580be40cc028c88ca512b0
Mihai Fufezan <mihai@fufexan.net>
2026-05-08 15:16:55 +0300

    hyprland: add springsss

--- system/programs/hyprland/animations.lua
+++ system/programs/hyprland/animations.lua
@@ -1,7 +1,14 @@
+----------------
+---- CURVES ----
+----------------
+
+hl.curve("bounce", { type = "spring", mass = 1, stiffness = 50, dampening = 10 })
+hl.curve("slight_bounce", { type = "spring", mass = 1, stiffness = 35, dampening = 10 })
+
 ----------------------
 ---- ANIMATIONS ------
 ----------------------
 hl.animation({ leaf = "border", enabled = true, speed = 2, bezier = "default" })
 hl.animation({ leaf = "fade", enabled = true, speed = 4, bezier = "default" })
-hl.animation({ leaf = "windows", enabled = true, speed = 3, bezier = "default", style = "popin 80%" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 2, bezier = "default", style = "slide" })
+hl.animation({ leaf = "windows", enabled = true, speed = 8, spring = "bounce", style = "popin 80%" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 2, spring = "slight_bounce", style = "slight_bounce" })
