204aaeec755bb09eaa360de41d6671271ab75939
Mihai Fufezan <mihai@fufexan.net>
2026-05-24 20:04:03 +0300

    home/services/gammastep: use geoclue

--- home/services/wayland/gammastep.nix
+++ home/services/wayland/gammastep.nix
@@ -4,9 +4,7 @@
     tray = true;
 
     # stopgap until geoclue's wifi location is fixed
-    provider = "manual";
-    latitude = 45.0;
-    longitude = 25.0;
+    provider = "geoclue2";
 
     enableVerboseLogging = true;
 
