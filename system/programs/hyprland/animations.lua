6fd0ac66735c8c1b601c67e5d67c4d998bafa9e5
Mihai Fufezan <mihai@fufexan.net>
2026-05-24 20:04:26 +0300

    hyprland/animations: fix anim

--- system/programs/hyprland/animations.lua
+++ system/programs/hyprland/animations.lua
@@ -11,4 +11,4 @@ hl.curve("slight_bounce", { type = "spring", mass = 1, stiffness = 35, dampening
 hl.animation({ leaf = "border", enabled = true, speed = 2, bezier = "default" })
 hl.animation({ leaf = "fade", enabled = true, speed = 4, bezier = "default" })
 hl.animation({ leaf = "windows", enabled = true, speed = 8, spring = "bounce", style = "popin 80%" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 2, spring = "slight_bounce", style = "slight_bounce" })
+hl.animation({ leaf = "workspaces", enabled = true, speed = 2, spring = "slight_bounce" })
