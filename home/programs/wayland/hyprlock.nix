8e498f4fc4c5811a384c7d6c6795542d25a98b14
Mihai Fufezan <mihai@fufexan.net>
2026-06-11 19:42:35 +0300

    hyprlock: fix warns, disable fade anim

--- home/programs/wayland/hyprlock.nix
+++ home/programs/wayland/hyprlock.nix
@@ -12,15 +12,13 @@
 
     settings = {
       general = {
-        disable_loading_bar = true;
         immediate_render = true;
         hide_cursor = false;
-        no_fade_in = true;
       };
 
       animation = [
         "inputFieldDots, 1, 2, linear"
-        "fadeIn, 0"
+        "fade, 0"
       ];
 
       background = [
@@ -32,7 +30,7 @@
 
       input-field = [
         {
-          monitor = "eDP-1";
+          monitor = "";
 
           size = "300, 50";
           valign = "bottom";
