d540918a53ed104c118c143b7cd858c9d5f6a880
Mihai Fufezan <mihai@fufexan.net>
2026-07-02 09:17:56 +0300

    home/programs/games: add rocket-league

--- home/programs/games/default.nix
+++ home/programs/games/default.nix
@@ -10,10 +10,9 @@
     (inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.osu-lazer-bin.override {
       pipewire_latency = "128/48000";
     })
+    inputs.nix-gaming.packages.${pkgs.stdenv.hostPlatform.system}.rocket-league
     gamescope
     prismlauncher
     # (lutris.override {extraPkgs = p: [p.libnghttp2];})
-    winetricks
-    protontricks
   ];
 }
