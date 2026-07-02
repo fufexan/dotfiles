0882ad70fcfc0320265874b5d8827b1a9e4170cf
Mihai Fufezan <mihai@fufexan.net>
2026-07-02 09:18:19 +0300

    system/nix/nixpkgs: disable grim patch for now
    causes grim to segfault when using `-T <stableId>`

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -6,23 +6,24 @@
 
     overlays = [
       (final: prev: {
-        grim = prev.grim.overrideAttrs (
-          _: super:
-          let
-            grim-patch = final.fetchFromGitHub {
-              owner = "thequantumcog";
-              repo = "grim-patch";
-              rev = "a1671f3e782ec677d168612ae8287dc55be0b5d4";
-              hash = "sha256-g7Rkp63ZBJp7DB+t38hQMCgsWM89uS6d4etaOkHa/XI=";
-            };
-          in
-          {
-            # Adds patch that fixes blurry screenshots
-            # https://github.com/thequantumcog/grim-patch
-            # Adapted from https://lists.sr.ht/~emersion/grim-dev/patches/56912
-            patches = (super.patches or [ ]) ++ [ "${grim-patch}/hyprland.patch" ];
-          }
-        );
+        # makes grim -T <stableId> crash
+        # grim = prev.grim.overrideAttrs (
+        #   _: super:
+        #   let
+        #     grim-patch = final.fetchFromGitHub {
+        #       owner = "thequantumcog";
+        #       repo = "grim-patch";
+        #       rev = "a1671f3e782ec677d168612ae8287dc55be0b5d4";
+        #       hash = "sha256-g7Rkp63ZBJp7DB+t38hQMCgsWM89uS6d4etaOkHa/XI=";
+        #     };
+        #   in
+        #   {
+        #     # Adds patch that fixes blurry screenshots
+        #     # https://github.com/thequantumcog/grim-patch
+        #     # Adapted from https://lists.sr.ht/~emersion/grim-dev/patches/56912
+        #     patches = (super.patches or [ ]) ++ [ "${grim-patch}/hyprland.patch" ];
+        #   }
+        # );
 
         linuxPackages_latest = prev.linuxPackages_latest.extend (
           _: lpprev: {
